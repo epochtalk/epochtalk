0a7a6763bcbffe73c09135cdc35f2f556244d55c
James Wang <jw@slickage.com>
2014-04-14 09:39:33 -1000

    Config root + all deps in public.

--- server/config.js
+++ server/config.js
@@ -1,4 +1,8 @@
+var path = require('path');
+var rootPath = path.normalize(__dirname + '/..');
+
 var config = {
+  root: rootPath,
   port: process.env.PORT || 8080,
   redis: {
     host: process.env.REDIS_HOST || '127.0.0.1',
