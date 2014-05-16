7e8b86bc905b4c9a9f9fbc1de11709d3009098da
James Wang <jw@slickage.com>
2014-05-16 13:09:34 -1000

    Seed test with charlatan.

--- couch/seed.js
+++ couch/seed.js
@@ -4,9 +4,29 @@ var dbName = config.couchdb.name;
 var ddoc = require(__dirname + '/../couch/ddoc');
 var dbUrl = config.couchdb.url + '/' + dbName;
 
+var Charlatan = require('charlatan');
 var seed = {};
 module.exports = seed;
 
 seed.seed = function() {
+  var db = nano.use(dbName);
   console.log(dbUrl);
+
+  var users = [];
+
+  for (var i = 0; i < 10; i++) {
+    var name = Charlatan.Name.name();
+    var email = Charlatan.Internet.email();
+    var company = Charlatan.Company.name();
+    var user = {
+      name: name,
+      email: email,
+      company: company,
+      type: 'user'
+    };
+    db.insert(user, function(err, body) {
+      console.log(body);
+    });
+  }
+
 };
