fa458d9532127231ac883c567ad767a60311d891
James Wang <jw@slickage.com>
2014-05-29 23:45:30 -1000

    Foundation css integration.

--- app/app.js
+++ app/app.js
@@ -15,4 +15,6 @@ var app = angular.module('ept', [
 app.config(require('./config'));
 app.controller('HeaderCtrl', require('./controllers/header'));
 
+require('./css/normalize.css');
+require('./css/foundation.css');
 require('./css/style.css');
