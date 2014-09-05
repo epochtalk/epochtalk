398803af3b3696ba63c62ca362c4b07efe9fa54d
Anthony Kinsey <anthony@slickage.com>
2014-09-05 13:37:18 -1000

    Adding child board thread/post count to parent board. UI Updates
    * Updated pagination to look nicer
    * Removed ID's from boards/posts/threads
    * Adding child boards thread/post count to parent boards total
    posts/thread count for boards view.

--- app/controllers/boards.js
+++ app/controllers/boards.js
@@ -1,6 +1,15 @@
 module.exports = ['$scope', '$http', '$rootScope', 'breadcrumbs',
   function($scope, $http, $rootScope, breadcrumbs) {
     $http.get('/api/boards').success(function(boards) {
+      boards.forEach(function(board) {
+        if (board.children && board.children.length > 0) {
+          // append all children counts
+          board.children.forEach(function(child) {
+            board.post_count += Number(child.post_count);
+            board.thread_count += Number(child.thread_count);
+          });
+        }
+      });
       $scope.boards = boards;
       $rootScope.breadcrumbs = breadcrumbs.get();
     });
