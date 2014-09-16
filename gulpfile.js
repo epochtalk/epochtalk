b947c2f71b4fc10b50a97cedcc5a202a483f3129
James Wang <jw@slickage.com>
2014-09-15 21:34:07 -1000

    Removing uglify as a separate module, now part of browserify task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var path = require('path');
 var gulp = require('gulp');
 var browserify = require(path.join(__dirname, 'gulp', 'browserify'));
-var uglify = require(path.join(__dirname, 'gulp', 'uglify'));
 
 gulp.task('build', ['browserify']);
 gulp.task('default', ['build']);
