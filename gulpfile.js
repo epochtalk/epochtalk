216a7c1aa17a8fa41e339eca97cd122a17dc41b2
James Wang <jw@slickage.com>
2014-09-18 23:30:02 -1000

    Gulp watch works with browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var path = require('path');
 var gulp = require('gulp');
 var browserify = require(path.join(__dirname, 'gulp', 'browserify'));
+var watch = require('gulp-watch');
 
 gulp.task('build', ['browserify']);
 gulp.task('default', ['build']);
+gulp.task('watch', function() {
+  gulp.watch('app/**/*.{js,html}', ['build']);
+  gulp.watch('server/**/*.js');
+});
+
