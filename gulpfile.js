4a57ff1c007ce427f36313432f16d97b7ef034be
James Wang <jw@slickage.com>
2014-09-19 00:16:38 -1000

    Gulp with livereload for designer style coding.

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var path = require('path');
 var gulp = require('gulp');
 var browserify = require(path.join(__dirname, 'gulp', 'browserify'));
 var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
 
 gulp.task('build', ['browserify']);
 gulp.task('default', ['build']);
 gulp.task('watch', function() {
-  gulp.watch('app/**/*.{js,html}', ['build']);
+  livereload.listen();
+  gulp.watch('app/**/*.{js,html}', ['build']).on('change', livereload.changed);
+  gulp.watch('public/**/*').on('change', livereload.changed);
   gulp.watch('server/**/*.js');
 });
 
