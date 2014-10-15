de9a3cd5ad28cf18c83041f09f890992c48b4044
James Wang <jw@slickage.com>
2014-10-14 16:08:05 -1000

    Sass compilation from gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var browserify = require(path.join(__dirname, 'gulp', 'browserify'));
 var watch = require('gulp-watch');
 var livereload = require('gulp-livereload');
+var sass = require('gulp-sass')
 
 gulp.task('build', ['browserify']);
 gulp.task('default', ['build']);
@@ -11,5 +12,11 @@ gulp.task('watch', function() {
   gulp.watch('app/**/*.{js,html,css}', ['build']).on('change', livereload.changed);
   gulp.watch('public/**/*').on('change', livereload.changed);
   gulp.watch('server/**/*.js');
+});
+
+gulp.task('sass', function () {
+  gulp.src('./app/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./app/css'));
 });
 
