0dab6b06bc24466ab94fd644291d647b9bc642f0
Anthony Kinsey <anthony@slickage.com>
2014-10-23 21:54:28 -1000

    Updated gulp to watch scss files instead of css. Removed unsed package * Removed gulp-watch as it wasn't being used anywhere * css files are no longer manually edited, updated to watch scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ require(path.join(__dirname, 'gulp', 'sass'));
 require(path.join(__dirname, 'gulp', 'browserify'));
 var gulp = require('gulp');
 var del = require('del');
-var watch = require('gulp-watch');
 var livereload = require('gulp-livereload');
 
 gulp.task('build', ['copy-css', 'sass', 'browserify']);
@@ -11,7 +10,7 @@ gulp.task('default', ['build']);
 
 gulp.task('watch', function() {
   livereload.listen();
-  gulp.watch('app/**/*.{js,html,css}', ['build']).on('change', livereload.changed);
+  gulp.watch('app/**/*.{js,html,scss}', ['build']).on('change', livereload.changed);
   gulp.watch('public/**/*').on('change', livereload.changed);
   gulp.watch('server/**/*.js');
 });
