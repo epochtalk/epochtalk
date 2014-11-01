21a2bd66f0ec3e18cba6b5ec9d5c6df61a92b67e
Edward Kim <ed@slickage.com>
2014-10-31 15:57:02 -1000

    Fixed scrolling in /boards
    Applied the same fix for the posts page. Basically just moved the
    $anchorScroll after the promise of boards is fulfilled.

--- app/controllers/boards.js
+++ app/controllers/boards.js
@@ -1,6 +1,10 @@
 module.exports = ['$timeout', '$anchorScroll', 'boards',
   function($timeout, $anchorScroll, boards) {
-    this.categorizedBoards = boards;
-    $timeout(function() { $anchorScroll(); }, 100); // 100 for quick clicks
+    var ctrl = this;
+
+    boards.$promise.then(function(allBoards) {
+      ctrl.categorizedBoards = allBoards;
+      $timeout($anchorScroll);
+    });
   }
 ];
