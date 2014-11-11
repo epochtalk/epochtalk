a71331f711ebc62b2ca45571177cccc4efcaf497
Edward Kim <ed@slickage.com>
2014-11-11 13:50:36 -1000

    Registration Page: Removed enter keypress to submit
    Removed all bindings in the input fields on the registration page
    (keypresses) because they skip the validation that is enforced by the
    form.

--- app/controllers/registration.js
+++ app/controllers/registration.js
@@ -1,24 +1,17 @@
 module.exports = ['$location', 'Auth',
   function($location, Auth) {
     var ctrl = this;
-    if (Auth.isAuthenticated()) {
-      $location.path('/');
-    }
+    
+    if (Auth.isAuthenticated()) { $location.path('/boards'); }
 
     this.user = {};
     this.error = {};
-
-    this.submit = function(keyEvent) {
-      if (keyEvent.which === 13) {
-        ctrl.register();
-      }
-    };
 
     this.register = function() {
       ctrl.error = {};
 
       Auth.register(this.user,
-        function() { $location.path('/'); },
+        function() { $location.path('/boards'); },
         function(err) {
           ctrl.error.status = true;
           ctrl.error.message = err.data.message;
