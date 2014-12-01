472b4e83c58d964be438cfc6a477a689e12e2d21
Anthony Kinsey <anthony@slickage.com>
2014-11-30 23:14:15 -1000

    Trying to implement pagination in subviews

--- app/config.js
+++ app/config.js
@@ -6,6 +6,83 @@ require('./resources');
 module.exports = ['$stateProvider', '$locationProvider', '$httpProvider',
   function($stateProvider, $locationProvider, $httpProvider) {
 
+    $stateProvider.state('test', {
+      url: '/boards',
+      abstract: true,
+      views: {
+        'header': { template: fs.readFileSync(__dirname + '/templates/header.html') },
+        'content': {
+          template: fs.readFileSync(__dirname + '/templates/test.html')
+        },
+        'footer': { template: fs.readFileSync(__dirname + '/templates/footer.html') },
+        'modals': { template: fs.readFileSync(__dirname + '/templates/modals.html') }
+      }
+    })
+    .state('test.sub', {
+      url: '/{boardId}?limit&page',
+      controller: 'BoardCtrl',
+      controllerAs: 'BoardCtrl',
+      resolve: {
+        board: ['Boards', '$stateParams', function(Boards, $stateParams) {
+          return Boards.get({ id: $stateParams.boardId});
+        }],
+        threads: ['Threads', '$stateParams', function(Threads, $stateParams) {
+          var query = {
+            board_id: $stateParams.boardId,
+            limit: Number($stateParams.limit) || 10,
+            page: Number($stateParams.page) || 1
+          };
+          return Threads.byBoard(query);
+        }],
+        page: ['$stateParams', function($stateParams) {
+          return Number($stateParams.page) || 1;
+        }],
+        threadLimit: ['$stateParams', function($stateParams) {
+          // TODO: this needs to be grabbed from user settings
+          return Number($stateParams.limit) || 10;
+        }],
+        postLimit: [function() {
+          // TODO: this needs to be grabbed from user settings
+          return 10;
+        }]
+      },
+      template: fs.readFileSync(__dirname + '/templates/boardData.html')
+    })
+    .state('test.sub.pagination', {
+      parent: 'test.sub',
+      controllerAs: 'ThreadCtrl',
+      reloadOnSearch: false,
+      controller: ['$scope', '$timeout', '$location', '$state', '$stateParams', '$window', 'Auth', 'board', 'page', 'threadLimit',
+      function($scope, $timeout, $location, $state, $stateParams, $window, Auth, board, page, threadLimit) {
+        console.log(page, threadLimit);
+        var ctrl = this;
+        this.loggedIn = Auth.isAuthenticated; // check Auth
+        this.page = page; // this page
+        this.threadLimit = threadLimit;
+
+        board.$promise.then(function(board) {
+          ctrl.pageCount = Math.ceil(board.thread_count / threadLimit);
+        });
+
+        $scope.$watchCollection('$stateParams', function() {
+          console.log('$stateParams', $stateParams);
+        });
+      }],
+      resolve: {
+        board: ['Boards', '$stateParams', function(Boards, $stateParams) {
+          return Boards.get({ id: $stateParams.boardId});
+        }],
+        page: ['$stateParams', function($stateParams) {
+          return Number($stateParams.page) || 1;
+        }],
+        threadLimit: ['$stateParams', function($stateParams) {
+          // TODO: this needs to be grabbed from user settings
+          return Number($stateParams.limit) || 10;
+        }]
+      },
+      template: fs.readFileSync(__dirname + '/templates/boardPagination.html')
+    });
+
     $stateProvider.state('index', {
       url: '/',
       views: {
@@ -83,14 +160,40 @@ module.exports = ['$stateProvider', '$locationProvider', '$httpProvider',
       }
     })
     .state('threads', {
-      url: '/boards/{boardId}?limit&page',
-      // reloadOnSearch: false,
+      url: '',
       views: {
         'header': { template: fs.readFileSync(__dirname + '/templates/header.html') },
         'content': {
+          abstract: true,
+          template: fs.readFileSync(__dirname + '/templates/board.html')
+        },
+        'footer': { template: fs.readFileSync(__dirname + '/templates/footer.html') },
+        'modals': { template: fs.readFileSync(__dirname + '/templates/modals.html') }
+      }
+    })
+
+    .state('threads.data', {
+      url: '',
+      views: {
+        'pagination@': {
+          controlerAs: 'BoardCtrl',
+          controller:  ['$scope', '$timeout', '$location', '$state', '$stateParams', '$window', 'Auth', 'board', 'page', 'threadLimit',
+          function($scope, $timeout, $location, $state, $stateParams, $window, Auth, board, page, threadLimit) {
+            var ctrl = this;
+            this.loggedIn = Auth.isAuthenticated; // check Auth
+            this.page = page; // this page
+            this.threadLimit = threadLimit;
+
+            board.$promise.then(function(board) {
+              ctrl.pageCount = Math.ceil(board.thread_count / threadLimit);
+            });
+          }],
+          template: fs.readFileSync(__dirname + '/templates/boardPagination.html'),
+        },
+        'data@': {
           controller: 'BoardCtrl',
           controllerAs: 'BoardCtrl',
-          template: fs.readFileSync(__dirname + '/templates/board.html'),
+          template: fs.readFileSync(__dirname + '/templates/boardData.html'),
           resolve: {
             board: ['Boards', '$stateParams', function(Boards, $stateParams) {
               return Boards.get({ id: $stateParams.boardId});
@@ -115,9 +218,7 @@ module.exports = ['$stateProvider', '$locationProvider', '$httpProvider',
               return 10;
             }]
           }
-        },
-        'footer': { template: fs.readFileSync(__dirname + '/templates/footer.html') },
-        'modals': { template: fs.readFileSync(__dirname + '/templates/modals.html') }
+        }
       }
     })
     .state('newThread', {
