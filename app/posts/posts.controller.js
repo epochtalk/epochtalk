d442a8f93983925a967c57704ae3463f15357f4e
Edward Kim <ed@slickage.com>
2014-12-09 16:21:31 -1000

    Pulled Editor into a fixed position modal
    This commit encapsulates all the work needed to pull the editor
    directive out into a fixed position modal. This includes updating the
    scss to contain the editor in a fixed position div as well as updating
    the styles to fit correctly.
    
    The controller was also updated to handle new and previous post editing
    into a single modal rather than having an editor for all posts on a
    page. This change also updated the controller so that there is only one
    method to set the properties of an editor and one method to save any
    changes to a new or old post.
    
    The posts.data.html was also updated to account for the single editor.

--- app/posts/posts.controller.js
+++ app/posts/posts.controller.js
@@ -1,19 +1,30 @@
-module.exports = ['$scope', '$timeout', '$location', '$stateParams', '$anchorScroll', 'Auth', 'Posts', 'thread', 'posts', 'page', 'limit',
-  function($scope, $timeout, $location, $stateParams, $anchorScroll, Auth, Posts, thread, posts, page, limit) {
+module.exports = ['$scope', '$timeout', '$location', '$anchorScroll', 'Auth', 'Posts', 'thread', 'posts', 'page', 'limit',
+  function($scope, $timeout, $location, $anchorScroll, Auth, Posts, thread, posts, page, limit) {
     var ctrl = this;
-    this.loggedIn = Auth.isAuthenticated; // used to check auth
-    var tempPosts = {}; // contains post content while editing
-    this.page = page; // current page
-    this.limit = limit; // posts per page
-    this.resetEditor = true; // used to reset new post editor
-    this.parent = $scope.$parent.PostsWrapperCtrl;
-    this.parent.page = page;
+    this.loggedIn = Auth.isAuthenticated;
+    var parent = $scope.$parent.PostsWrapperCtrl;
+    parent.page = page;
+    
+    this.resetEditor = true;
+    this.showEditor = false;
+    this.tempPost = {
+      thread_id: '',
+      title: '',
+      body: '',
+      encodedBody: ''
+    };
+    this.posting = {
+      type: '',
+      id: '',
+      index: '',
+      error: '',
+      post: this.tempPost,
+    };
 
-    // set the thread, newPost.thread_id and title
-    // and calculate the total number of pages
+    // set the thread, new post, and calculate the total number of pages
     thread.$promise.then(function(thread) {
       ctrl.thread = thread;
-      ctrl.newPost = { title: 'Re: ' + thread.title, thread_id: thread.id };
+      ctrl.writePost();
       ctrl.totalPosts = thread.post_count;
       calculatePages();
     });
@@ -24,8 +35,6 @@ module.exports = ['$scope', '$timeout', '$location', '$stateParams', '$anchorScr
       $timeout($anchorScroll);
     });
 
-    // scroll to any hash in the url
-
     // figure out how many pages there should be in this thread
     // Should be called only after the total number of posts this thread
     // has is already determined or recalculated.
@@ -33,121 +42,72 @@ module.exports = ['$scope', '$timeout', '$location', '$stateParams', '$anchorScr
       var count;
       if (ctrl.limit === 'all') { count = Number(ctrl.totalPosts); }
       else { count = Number(ctrl.limit) || 10; }
-      ctrl.pageCount = Math.ceil(ctrl.totalPosts / count);
-      ctrl.parent.pageCount = ctrl.pageCount;
+      parent.pageCount = Math.ceil(ctrl.totalPosts / count);
     };
 
-    // new post methods
+    this.writePost = function(index) {
+      var post = ctrl.posts && ctrl.posts[index] || '';
+      if (post) {
+        ctrl.posting.type = 'edit';
+        ctrl.posting.index = index;
+      }
+      else {
+        ctrl.posting.type = 'new';
+        ctrl.posting.index = '';
+      }
+      
+      ctrl.posting.error = '';
+      ctrl.posting.id = post.id || '';
+      var editorPost = ctrl.posting.post;
+      editorPost.thread_id = post.thread_id || ctrl.thread.id;
+      editorPost.title = post.title || 'Re: ' + ctrl.thread.title;
+      editorPost.body = post.body || '';
+      editorPost.encodedBody = post.encodedBody || '';
+      
+      if (post) { ctrl.showEditor = true; }
+      ctrl.resetEditor = true;
+    };
 
     this.savePost = function() {
-      delete ctrl.newPost.error;
+      var type = ctrl.posting.type;
+      var post = ctrl.posting.post;
+      var postPromise;
 
-      Posts.save(ctrl.newPost).$promise
-      .then(function(post) {
-        // Increment post count and recalculate ctrl.pageCount
-        ctrl.totalPosts++;
-        calculatePages();
+      if (type === 'new') {
+        postPromise = Posts.save(post).$promise;
+      }
+      else if (type === 'edit') {
+        postPromise = Posts.update({id: ctrl.posting.id}, post).$promise;
+      }
 
-        // clean out new post
-        ctrl.newPost.body = '';
-        ctrl.newPost.encodedBody = '';
-        ctrl.resetEditor = true; // reset editor
-
-        // Go to last page in the thread
-        // and scroll to new post
-        $location.search('page', ctrl.pageCount);
-        $location.hash(post.id); // set post id in url hash for scrolling
+      postPromise.then(function(data) {
+        if (type === 'new') {
+          // Increment post count and recalculate ctrl.pageCount
+          ctrl.totalPosts++;
+          calculatePages();
+          // Go to last page in the thread and scroll to new post
+          $location.search('page', parent.pageCount);
+          $location.hash(data.id); // set post id in url hash for scrolling
+        }
+        else if (type === 'edit') {
+          var index = ctrl.posting.index;
+          ctrl.posts[index].body = data.body;
+          ctrl.posts[index].encodedBody = data.encodedBody;
+        }
+        
+        ctrl.writePost(); // reset editor
       })
       .catch(function(response) {
         var error = '';
-        if (response.status === 500) {
+        if (response.status == 500) {
           error = 'Post could not be saved. ';
           error += 'Please refresh the page.';
         }
         else { error = response.data.message; }
 
-        ctrl.newPost.error = {};
-        ctrl.newPost.error.message = error;
+        ctrl.posting.error = {};
+        ctrl.posting.error.message = error;
       });
     };
-
-    // edit post methods
-
-    this.startEditPost = function(index) {
-      var editPost = ctrl.posts[index];
-
-      // save a copy in case they cancel
-      tempPosts[editPost.id] = {};
-      tempPosts[editPost.id].title = editPost.title;
-      tempPosts[editPost.id].body = editPost.body;
-      tempPosts[editPost.id].encodedBody = editPost.encodedBody;
-
-      // turn on editing
-      ctrl.posts[index].editMode = true;
-    };
-
-    this.saveEditPost = function(index) {
-      var editPost = ctrl.posts[index]; // get post content
-      delete editPost.error; // delete any errors for this post
-
-      // create updated version of this post
-      var saveEditPost = {
-        title: editPost.title,
-        encodedBody: editPost.encodedBody,
-        thread_id: editPost.thread_id
-      };
-
-      // away we go!
-      Posts.update({id: editPost.id}, saveEditPost).$promise
-      .then(function(data) {
-        // remove temp post since post has been saved
-        delete tempPosts[editPost.id];
-
-        editPost.body = data.body;
-        editPost.encodedBody = data.encodedBody;
-
-        // switch off edit mode
-        editPost.editMode = false;
-      })
-      .catch(function(response) {
-        var error = '';
-        if (response.status === 500) {
-          error = 'Post was not able to be saved. ';
-          error += 'Please refresh the page.';
-        }
-        else { error = response.data.message; }
-
-        ctrl.posts[index].error = {};
-        ctrl.posts[index].error.message = error;
-      });
-    };
-
-    this.cancelEditPost = function(index) {
-      var cancelPost = ctrl.posts[index];
-
-      // replace post content with content from tempPost
-      ctrl.posts[index].title = tempPosts[cancelPost.id].title;
-      ctrl.posts[index].body = tempPosts[cancelPost.id].body;
-      ctrl.posts[index].encodedBody = tempPosts[cancelPost.id].encodedBody;
-      ctrl.posts[index].editMode = false; // turn off editing
-
-      delete tempPosts[cancelPost.id]; // delete this content in tempPosts
-    };
-
-    // // pagination
-
-    // $scope.$on('$stateChangeStart', function() {
-    //   var limit = ctrl.limit === 'all' ? Number(ctrl.totalPosts) : $stateParams.limit;
-    //   var query = {
-    //     thread_id: $stateParams.threadId,
-    //     limit: limit,
-    //     page: $stateParams.page
-    //   };
-    //   return Posts.byThread(query).$promise.then(function(posts) {
-    //     ctrl.posts = posts;
-    //     ctrl.page = Number($stateParams.page);
-    //     $timeout($anchorScroll);
-    //   });
-    // });
   }
 ];
