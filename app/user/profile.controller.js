3a0f152c7d0c14e986a69a3f3263d4bf30f1a8a0
Anthony Kinsey <anthony@slickage.com>
2014-12-22 12:29:51 -1000

    Fix for profiles page DOB/Age field. * DOB was coming back as a date time string from the db,   now prior to display to the user, it is formatted as a   human readable date. * Upon saving the profile, the field would change to 'Age'   but still only display the dob, this is now resolved. The   age is now calculated from the users entered DOB.

--- app/user/profile.controller.js
+++ app/user/profile.controller.js
@@ -1,5 +1,5 @@
-module.exports = ['user', 'User', 'Auth', '$location', '$timeout',
-  function(user, User, Auth, $location, $timeout) {
+module.exports = ['user', 'User', 'Auth', '$location', '$timeout', '$filter',
+  function(user, User, Auth, $location, $timeout, $filter) {
     var ctrl = this;
 
     // Edit Profile Fields
@@ -11,6 +11,11 @@ module.exports = ['user', 'User', 'Auth', '$location', '$timeout',
       ctrl.user = user;
       ctrl.displayUsername = angular.copy(user.username);
       ctrl.displayEmail = angular.copy(user.email);
+      ctrl.user.dob = $filter('date')(ctrl.user.dob, 'longDate');
+      ctrl.userAge = calcAge(ctrl.user.dob);
+
+      // This isn't the profile users true local time, just a placeholder
+      ctrl.userLocalTime = $filter('date')(Date.now(), 'medium');
     });
 
     this.saveProfile = function() {
@@ -19,6 +24,10 @@ module.exports = ['user', 'User', 'Auth', '$location', '$timeout',
         ctrl.user = data;
         ctrl.editMode = false;
         ctrl.error = {}; // reset error
+
+        // Reformat DOB and calculate age on save
+        ctrl.user.dob = $filter('date')(ctrl.user.dob, 'longDate');
+        ctrl.userAge = calcAge(ctrl.user.dob);
 
         // redirect page if username changed
         if (ctrl.displayUsername !== ctrl.user.username) {
@@ -79,6 +88,15 @@ module.exports = ['user', 'User', 'Auth', '$location', '$timeout',
         ctrl.changePassStatus.message = err.data.message;
         ctrl.changePassStatus.type = 'alert';
       });
+    };
+
+    // Helper methods
+    var calcAge = function(dob) {
+      if (!dob) { return '';}
+      dob = new Date(dob);
+      var ageDiff = Date.now() - dob.getTime();
+      var ageDate = new Date(ageDiff);
+      return Math.abs(ageDate.getUTCFullYear() - 1970);
     };
 
   }
