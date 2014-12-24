ed4dd266f6db0501e98ddf0a62a3e8f8b24c8d68
Anthony Kinsey <anthony@slickage.com>
2014-12-23 17:20:17 -1000

    Fix for scroll lock directive * Scroll lock now accepts a boolean parameter, when false, removes   the scroll lock class from the body. * This commit fixes issue with locked scrolling after posting

--- app/components/scroll_lock/scroll-lock.directive.js
+++ app/components/scroll_lock/scroll-lock.directive.js
@@ -2,8 +2,14 @@ module.exports = ['$document',
   function($document) {
     return {
       restrict: 'A',
-      link: function(scope, element) {
+      link: function(scope, element, attributes) {
+
         var body = $document[0].body;
+
+        // Remove scroll lock if scroll-lock = false
+        scope.$watch(attributes.scrollLock, function(enabled) {
+          if (!enabled) { body.classList.remove('body-scroll-lock'); }
+        });
 
         element.on('mouseenter', function() {
           body.classList.add('body-scroll-lock');
