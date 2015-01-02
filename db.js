66e8bf64272acdb7a9d4c52db126483e75806889
Bronson Oka <boka@slickage.com>
2015-01-02 13:28:36 -1000

    db now uses config file for database name

--- db.js
+++ db.js
@@ -1,1 +1,8 @@
-module.exports = require('epoch-core-pg')();
+var path = require('path');
+var config = require(path.join(__dirname, 'config.json'));
+if (config) {
+  var options = {
+    db: config.db || undefined
+  };
+}
+module.exports = require('epoch-core-pg')(options);
