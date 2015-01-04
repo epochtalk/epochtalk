23410d633ed58f7ceaf90f6e2468bb612f06377d
James Wang <jw@slickage.com>
2015-01-03 19:52:49 -1000

    Deploy updated for systemd.

--- flightplan.js
+++ flightplan.js
@@ -41,13 +41,14 @@ plan.remote(function(remote) {
   remote.log('Move folder to root');
   remote.sudo('cp -R /tmp/' + tmpDir + ' ~', {user: username});
   remote.rm('-rf /tmp/' + tmpDir);
-
   remote.log('Install dependencies');
   remote.sudo('npm --production --prefix ~/' + tmpDir + ' install ~/' + tmpDir, {user: username});
-
   remote.log('Reload application');
   remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
-  remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
-  remote.exec('forever start ~/'+appName+'/'+startFile);
+  remote.exec('cp ~/btctalk.env ~/' + appName + '/.env');
+  remote.exec('foreman export systemd /tmp/ -a ' + appName);
+  remote.sudo('cp /tmp/' + appName + '*.service /tmp/' + appName + '*.target /usr/lib/systemd/system/');
+  remote.sudo('systemctl enable ' + appName + '.target');
+  remote.sudo('systemctl stop ' + appName + '.target');
+  remote.sudo('systemctl start ' + appName + '.target');
 });
-
