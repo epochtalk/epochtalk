6697c7176e460575dd4962cd5af36778e079c2fc
Edward Kim <ed@slickage.com>
2015-01-07 18:16:42 -1000

    Added use of editor dirty flag to new thread page
    Added the user of the editor's dirty flag to the new thread page. This
    also required adding the user of the reset-switch attribute because the
    way the editor is initiated has changed.
    
    The dirty flag is primarily used to help notify the submit button's
    validation.

--- app/board/new-thread.controller.js
+++ app/board/new-thread.controller.js
@@ -3,6 +3,8 @@ module.exports = ['$stateParams', '$location', 'Auth', 'Threads',
     var ctrl = this;
 
     this.exitEditor = false;
+    this.dirtyEditor = false;
+    this.resetEditor = true;
     this.error = {};
     this.thread = {
       board_id: $stateParams.boardId,
