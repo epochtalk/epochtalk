baadec5863b288ac85c31af933dbc011de34b338
James Wang <jw@slickage.com>
2015-01-14 18:56:58 -1000

    Server modularized to epochtalk/server

--- server/routes/breadcrumbs/index.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-var breadcrumbs = require(path.join(__dirname, 'config'));
-
-// Export Routes
-module.exports = [
-  { method: 'GET', path: '/breadcrumbs', config: breadcrumbs.byType }
-];
