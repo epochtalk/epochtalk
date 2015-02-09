08a0f8ec43a97ee696ae2ac04cfacaf5e1835e12
James Wang <jw@slickage.com>
2015-02-08 20:43:00 -1000

    Confirm account redirects to home instead boards

--- app/user/confirm.controller.js
+++ app/user/confirm.controller.js
@@ -1,17 +1,15 @@
 module.exports = ['$location', '$timeout', '$stateParams', 'Auth', function($location, $timeout, $stateParams, Auth) {
   var ctrl = this;
   this.message = '';
-
   var params = {
     username: $stateParams.username,
     token: $stateParams.token
   };
-
   Auth.confirmAccount(params, function() {
     ctrl.message = 'Account successfully confirmed, you will be redirected shortly...';
-    $timeout(function() { $location.path('/boards'); }, 3000);
+    $timeout(function() { $location.path('/'); }, 3000);
   }, function(err) {
     ctrl.message = err.data.message;
-    $timeout(function() { $location.path('/boards'); }, 3000);
+    $timeout(function() { $location.path('/'); }, 3000);
   });
 }];
