c6b9cd48029cb3ba7a2b5e47e32266dab985a661
Edward Kim <ed@slickage.com>
2015-04-21 15:12:58 -1000

    Manual trigger for auto-focus directive
    Added a scope model that allows the auto-focus directive to bind to a
    model. This allows any parent scope object to be directly bind to the
    auto-focus directive and dictate to it when it should re-focus.

--- app/components/autofocus/autofocus.directive.js
+++ app/components/autofocus/autofocus.directive.js
@@ -1,8 +1,15 @@
 module.exports = ['$timeout', function($timeout) {
   return {
     restrict: 'AC',
+    scope: { autoFocus: '=' },
     link: function(scope, element) {
-      $timeout(function() { element[0].focus(); }, 300);
+      function focus() { element[0].focus(); }
+
+      $timeout(focus, 300);
+
+      scope.$watch('autoFocus', function(trigger) {
+        if (trigger) { $timeout(focus, 300); }
+      });
     }
   };
 }];
