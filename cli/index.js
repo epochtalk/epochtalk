3334c47cf2674953216bf618c1bde4645e8d8af1
James Wang <jw@slickage.com>
2015-05-31 21:06:12 -1000

    Seed moved to cli --create.

--- cli/index.js
+++ cli/index.js
@@ -1,19 +1,48 @@
 var path = require('path');
 var program = require('commander');
+var db = require(path.normalize(__dirname + '/../db'));
 
 program
   .version('0.0.1')
-  .option('--create [database]', 'Recreate [database]. Default: config.json{database} OR epochtalk)')
+  .option('--create', 'Create database.')
   .parse(process.argv);
 
 var genericArgs = {
   debug: program.debug,
   verbose: program.verbose,
-  db: program.leveldb || path.join(process.env.PWD, 'epoch.db')
 };
 
 if (program.create) {
-  console.log('asdf');
+  var adminUser = {
+    username: 'admin',
+    email: 'admin@epochtalk.com',
+    password: 'admin1234',
+    confirmation: 'admin1234'
+  }
+  db.users.create(adminUser, true)
+  .then(function() {
+    var generalCategory = { name: 'General' };
+    return db.categories.create(generalCategory);
+  })
+  .then(function(category) {
+    console.log('Created category: ' + category.name);
+    var generalBoard = {
+      category_id: category.id,
+      name: 'General Discussion',
+      description: 'The art of discussing, generally.'
+    };
+    return db.boards.create(generalBoard);
+  })
+  .then(function(board) {
+    console.log('Created board: ' + board.name);
+  })
+  .then(function() {
+    db.close();
+  })
+  .catch(function(err) {
+    console.log(err);
+    db.close();
+  });
 }
 else {
   program.help();
