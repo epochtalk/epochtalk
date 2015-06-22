4be63aff20ef3d22a767e70be647abe3cd22fe6a
Anthony Kinsey <anthony@slickage.com>
2015-06-22 12:12:32 -1000

    Updated category update route since merging in board map changes

--- server/routes/boards/config.js
+++ server/routes/boards/config.js
@@ -188,13 +188,19 @@ exports.allCategories = {
   * @api {POST} /boards/categories Update Categories
   * @apiName UpdateCategories
   * @apiPermission Super Administrator, Administrator
-  * @apiDescription Upsert for create/modifying boards within their categories.
+  * @apiDescription Used to update boards within their categories.
   *
-  * @apiParam (Payload) {array} categories Array containing all of the forums boards in their respective categories
+  * @apiParam (Payload) {object[]} boardMapping Array containing mapping of boards and categories
+  * @apiParam (Payload) {string} boardMapping.id The id of the category or board
+  * @apiParam (Payload) {string} boardMapping.name The name of the category or board
+  * @apiParam (Payload) {string="board","category"} boardMapping.type The type of the mapping object
+  * @apiParam (Payload) {number} boardMapping.view_order The view order of the board or category
+  * @apiParam (Payload) {string} [boardMapping.category_id] If type is "board" the id of the category the board belongs to
+  * @apiParam (Payload) {string} [boardMapping.parent_id] If type is "board" and the board is a child board, the id of the parent board
   *
-  * @apiSuccess {array} categories Array containing all of the forums boards in their respective categories
+  * @apiSuccess {array} operations Array containing all of the operations performed while updating categories
   *
-  * @apiError (Error 500) InternalServerError There was an issue retrieving categories
+  * @apiError (Error 500) InternalServerError There was an issue updating categories/boards
   */
 exports.updateCategories = {
   auth: { mode: 'required', strategy: 'jwt' },
