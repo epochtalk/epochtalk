7a104c7da3ca61776ef8e5a7e7a92c7dcf7a5da4
Bronson Oka <boka@slickage.com>
2015-10-21 19:25:15 -1000

    create default admin configurations
    on server start, if configurations are not loaded, create with default
    options

--- setup.js
+++ setup.js
@@ -3,56 +3,64 @@ var config = require(path.normalize(__dirname + '/config'));
 var db = require(path.normalize(__dirname + '/db'));
 var defaultConfigurations = require(path.join(__dirname, 'configurations.json'));
 
-// load server options from database
+// load admin options from database
 module.exports = function() {
-  return db.configurations.get().then(function(configurations) {
-    Object.keys(configurations).forEach(function(key) {
-      config[key] = configurations[key];
-    });
-    // parse public url
-    var publicUrl = config.publicUrl;
-    if (publicUrl.indexOf('/', publicUrl.length-1) === publicUrl.length-1) {
-      config.publicUrl = publicUrl.substring(0, publicUrl.length-1);
-    }
-
-    // parse images local dir
-    var localDir = config.images.local.dir;
-    if (localDir.indexOf('/') !== 0) {
-      config.images.local.dir = '/' + localDir;
-      localDir =  '/' + localDir;
-    }
-    if (localDir.indexOf('/', localDir.length-1) === -1) {
-      config.images.local.dir = localDir + '/';
-    }
-    // parse images public dir
-    var localPath = config.images.local.path;
-    if (localPath.indexOf('/') !== 0) {
-      config.images.local.path = '/' + localPath;
-      localPath = '/' + localPath;
-    }
-    if (localPath.indexOf('/', localPath.length-1) === -1) {
-      config.images.local.path = localPath + '/';
-    }
-
-
-    // parse images root and dir
-    var s3root = config.images.s3.root;
-    if (s3root.indexOf('/', s3root.length-1) === -1) {
-      config.images.s3.root = s3root + '/';
-    }
-    var s3dir = config.images.s3.dir;
-    if (s3dir.indexOf('/', s3dir.length-1) === -1) {
-      s3dir += '/';
-      config.images.s3.dir = s3dir;
-    }
-    if (s3dir.indexOf('/') === 0) {
-      config.images.s3.dir = s3dir.substring(1);
-    }
-  })
+  return db.configurations.get().then(parseConfigs)
   .catch(function(err) {
+    // if admin options are not yet configured
     if (err.isOperational) {
-      return db.configurations.create(defaultConfigurations);
+      console.log('Not configured yet; using default configurations...');
+      // create with defaults
+      return db.configurations.create(defaultConfigurations).then(function() {
+        // then load admin options from database
+        return db.configurations.get().then(parseConfigs);
+      });
     }
     else { return console.log(err) }
   });
 };
+
+function parseConfigs(configurations) {
+  Object.keys(configurations).forEach(function(key) {
+    config[key] = configurations[key];
+  });
+  // parse public url
+  var publicUrl = config.publicUrl;
+  if (publicUrl.indexOf('/', publicUrl.length-1) === publicUrl.length-1) {
+    config.publicUrl = publicUrl.substring(0, publicUrl.length-1);
+  }
+
+  // parse images local dir
+  var localDir = config.images.local.dir;
+  if (localDir.indexOf('/') !== 0) {
+    config.images.local.dir = '/' + localDir;
+    localDir =  '/' + localDir;
+  }
+  if (localDir.indexOf('/', localDir.length-1) === -1) {
+    config.images.local.dir = localDir + '/';
+  }
+  // parse images public dir
+  var localPath = config.images.local.path;
+  if (localPath.indexOf('/') !== 0) {
+    config.images.local.path = '/' + localPath;
+    localPath = '/' + localPath;
+  }
+  if (localPath.indexOf('/', localPath.length-1) === -1) {
+    config.images.local.path = localPath + '/';
+  }
+
+
+  // parse images root and dir
+  var s3root = config.images.s3.root;
+  if (s3root.indexOf('/', s3root.length-1) === -1) {
+    config.images.s3.root = s3root + '/';
+  }
+  var s3dir = config.images.s3.dir;
+  if (s3dir.indexOf('/', s3dir.length-1) === -1) {
+    s3dir += '/';
+    config.images.s3.dir = s3dir;
+  }
+  if (s3dir.indexOf('/') === 0) {
+    config.images.s3.dir = s3dir.substring(1);
+  }
+}
