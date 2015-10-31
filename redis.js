dbeb33ba196be75a0a8104f74c82179ed6b92808
Bronson Oka <boka@slickage.com>
2015-10-30 16:23:41 -1000

    connect via config.redis.url if available

--- redis.js
+++ redis.js
@@ -2,5 +2,11 @@ var path = require('path');
 var config = require(path.normalize(__dirname + '/config'));
 var Promise = require('bluebird');
 var redis = Promise.promisifyAll(require('redis'));
-var redisClient = redis.createClient(config.redis.port, config.redis.host, config.redis.options);
+var redisClient;
+if (config.redis.url) {
+  redisClient = redis.createClient(config.redis.url, config.redis.options);
+}
+else {
+  redisClient = redis.createClient(config.redis.port, config.redis.host, config.redis.options);
+}
 module.exports = redisClient;
