ec7d2b0f3d174d8bc7cacce86216be1ebf6d7f89
Bronson Oka <boka@slickage.com>
2015-11-16 16:41:57 -1000

    allow non-array return from boards.query()

--- app/resources/boards.js
+++ app/resources/boards.js
@@ -4,6 +4,15 @@
 module.exports = ['$resource',
   function($resource) {
     return $resource('/api/boards/:id', {}, {
+      all: {
+        method: 'GET',
+        url: '/api/boards/all',
+        isArray: true
+      },
+      query: {
+        method: 'GET',
+        url: '/api/boards'
+      },
       update: {
         method: 'POST',
         params: { id: '@id' }
