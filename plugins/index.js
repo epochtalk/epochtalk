a86cd0060f7fadf618824d3628bfc2052e8af2f1
Bronson Oka <boka@slickage.com>
2015-11-20 17:47:51 -1000

    require fs in plugins/index

--- plugins/index.js
+++ plugins/index.js
@@ -2,6 +2,7 @@ var plugins = {};
 module.exports = plugins;
 
 var _ = require('lodash');
+var fs = require('fs');
 var path = require('path');
 var exec = require('child_process').exec;
 var db = require(path.normalize(__dirname + '/../db'));
