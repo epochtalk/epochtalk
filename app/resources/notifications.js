b69e593b697cdc787cd64e36992edc65ac13b93d
unenglishable <unenglishable@gmail.com>
2016-02-19 17:38:49 -1000

    add api/notifications/dismiss resource

--- app/resources/notifications.js
+++ app/resources/notifications.js
@@ -4,6 +4,11 @@
 module.exports = ['$resource',
   function($resource) {
     return $resource('/api/notifications/:id', {}, {
+      dismiss: {
+        params: { type: '@type' },
+        method: 'GET',
+        url: '/api/notifications/dismiss'
+      },
       counts: {
         method: 'GET',
         url: '/api/notifications/counts'
