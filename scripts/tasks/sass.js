4a710e0669a530f79c0d6d52a581f86d661ea7b9
Edward Kim <ed@slickage.com>
2016-03-02 15:28:39 -1000

    Node-sass-globbing added to support modular CSS
    The NPM lib, 'node-sass-globbing' was added so that modular CSS can be
    supported in the future.

--- scripts/tasks/sass.js
+++ scripts/tasks/sass.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var sass = require('node-sass');
 var Promise = require('bluebird');
+var nodeSassGlobbing = require('node-sass-globbing');
 
 // file params
 var sassPath = './app/scss/app.scss';
@@ -9,14 +10,23 @@ var publicSassPath = './public/css/app.css';
 module.exports = function(publicPath) {
   var currentSassPath = publicPath || publicSassPath;
   return new Promise(function(resolve, reject) {
-    var opts = { file: sassPath };
-    var output = sass.renderSync(opts);
-    fs.writeFile(currentSassPath, output.css, function(err) {
-      if (err) { reject(err); }
-      else {
-        console.log('SASS Compilation Complete.');
-        return resolve();
-      }
+    var opts = {
+      file: sassPath,
+      outputStyle: 'compressed',
+      importer: nodeSassGlobbing
+    };
+    sass.render(opts, function(error, result) {
+      if (error) { return reject(error); }
+      else { return resolve(result); }
     });
-  });
+  })
+  .then(function(output) {
+    return new Promise(function(resolve, reject) {
+      fs.writeFile(currentSassPath, output.css, function(err) {
+        if (err) { reject(err); }
+        else { return resolve(); }
+      });
+    });
+  })
+  .then(function() { console.log('SASS Compilation Complete'); });
 };
