d2fd5b86001159c4b413320b3621c125451ef8eb
Bronson Oka <boka@slickage.com>
2016-03-07 16:36:46 -1000

    add WEBSOCKET configuration in .env
    WEBSOCKET_HOST
    WEBSOCKET_PORT

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ var config = {
   publicUrl: process.env.PUBLIC_URL,
   host: process.env.HOST,
   port: process.env.PORT,
+  websocket_host: process.env.WEBSOCKET_HOST,
+  websocket_port: process.env.WEBSOCKET_PORT,
   redis: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
