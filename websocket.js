63c66b2ee196b17d6f2b782dabb1e1db539e047a
Bronson Oka <boka@slickage.com>
2016-03-21 15:47:21 -1000

    allow server websocket reconnect
    options for reconnect on the server is quicker than for clients

--- websocket.js
+++ websocket.js
@@ -3,5 +3,13 @@ var config = require(path.join(__dirname, 'config'));
 var socketcluster = require('socketcluster-client');
 module.exports = socketcluster.connect({
   hostname: config.websocket_host,
-  port: config.websocket_port
-});
+  port: config.websocket_port,
+  autoReconnect: true,
+  autoReconnectOptions: {
+    initialDelay: 1000,
+    randomness: 1000,
+    multiplier: 1.5,
+    maxDelay: 60000
+  }
+})
+.on('error', function(){});
