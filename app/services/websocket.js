27de36800e2e41c4f19845e6921606b89dd2e313
Bronson Oka <boka@slickage.com>
2016-03-21 15:46:21 -1000

    allow client websocket reconnect

--- app/services/websocket.js
+++ app/services/websocket.js
@@ -7,10 +7,10 @@ module.exports = ['$window', 'Session',
     var socketcluster = require('socketcluster-client');
     var socket = socketcluster.connect({
       hostname: forumData.websocket_host,
-      port: forumData.websocket_port
+      port: forumData.websocket_port,
+      autoReconnect: true
     })
     .on('error', function(err) {
-      socket.disconnect();
       console.log('Oh hey, a websocket error ocurred!', err);
     });
     return socket;
