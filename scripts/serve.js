3abef98cc4b3f5ed36f97ce100484640555512c7
Edward Kim <ed@slickage.com>
2016-03-24 15:51:22 -1000

    Added file watching for frontend and backend code
    These watchers will watch the files in /modules/node_module filtered by
    the given project names. This is to ensure that if work in done in other
    repos, as long as they are symlinked to this repo, the changes will
    propagate up and will cause either nodemon to refresh or have the
    changed files copied to the app dir. This only happens if the scripts
    are called while in a development environment.

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,13 +1,15 @@
 require('dotenv').load();
 var path = require('path');
-var nodemon = require('nodemon');
 var sass = require(path.join(__dirname, 'tasks', 'sass'));
 var clean = require(path.join(__dirname, 'tasks', 'clean'));
 var pack = require(path.join(__dirname, 'tasks', 'webpack'));
 var symlink = require(path.join(__dirname, 'tasks', 'symlink'));
 var copy_css = require(path.join(__dirname, 'tasks', 'copy_files'));
+var watchFiles = require(path.join(__dirname, 'tasks', 'watching'));
 var livereload = require(path.join(__dirname, 'tasks', 'livereload'));
 var installModules = require(path.join(__dirname, 'tasks', 'load_modules'));
+
+
 clean()
 .then(installModules)
 .then(copy_css)
@@ -15,25 +17,9 @@ clean()
 .then(symlink)
 .then(pack)
 .then(livereload)
-.then(function() { // nodemon
+.then(function() {
   if (process.env.NODE_ENV === 'production') {
     require(path.join(__dirname, '..', 'server', 'index'));
   }
-  else {
-    var nmOpts = {
-      script: './server/index.js',
-      ignore: [
-        './app',
-        './cli',
-        './public',
-        './repl',
-        './scripts',
-        './tests',
-      ]
-    };
-    nodemon(nmOpts);
-    process.once('SIGINT', function() {
-      process.exit(0);
-    });
-  }
+  else { watchFiles(); }
 });
---
+++ scripts/tasks/watching.js
@@ -0,0 +1,79 @@
+var path = require('path');
+var watch = require('watch');
+var nodemon = require('nodemon');
+var Promise = require('bluebird');
+var installModules = require(path.normalize(__dirname + '/load_modules'));
+
+var monitors = [];
+var nmPath = path.normalize(__dirname + '/../../modules/node_modules');
+var backendDirs = [
+  'ept-boards',
+  'ept-threads',
+  'ept-posts',
+  'ept-users',
+  'epochtalk-core-pg'
+];
+var frontendDir = 'ept-frontend';
+
+module.exports = function() {
+  // deploy nodemon first
+  var nm = launchNodemon();
+
+  // watch backend dirs
+  backendDirs.map(function(dir) {
+    return monitorBEDir(dir, nm)
+    .then(function(monitor) { monitors.push(monitor); });
+  });
+
+  // watch frontend dir
+  monitorFEDir(frontendDir)
+  .then(function(monitor) { monitors.push(monitor); });
+
+  // exiting
+  process.once('SIGINT', function() {
+    monitors.map(function(monitor) { monitor.stop(); });
+    process.exit(0);
+  });
+};
+
+var launchNodemon = function() {
+  var nmOpts = {
+    script: './server/index.js',
+    watch: [ './server', ],
+    ignore: [
+      './app',
+      './cli',
+      './content',
+      './public',
+      './repl',
+      './scripts',
+      './tests',
+    ]
+  };
+  return nodemon(nmOpts);
+};
+
+// watching the watcher
+var monitorBEDir = function(dir, nm) {
+  return new Promise(function(resolve) {
+    var dirPath = nmPath + '/' + dir;
+    watch.createMonitor(dirPath, function(monitor) {
+      monitor.on('created', function() { nm.emit('restart'); });
+      monitor.on('changed', function() { nm.emit('restart'); });
+      monitor.on('removed', function() { nm.emit('restart'); });
+      return resolve(monitor);
+    });
+  });
+};
+
+var monitorFEDir = function(dir) {
+  return new Promise(function(resolve) {
+    var dirPath = nmPath + '/' + dir;
+    watch.createMonitor(dirPath, function(monitor) {
+      monitor.on('created', function() { installModules(); });
+      monitor.on('changed', function() { installModules(); });
+      monitor.on('removed', function() { installModules(); });
+      return resolve(monitor);
+    });
+  });
+};
