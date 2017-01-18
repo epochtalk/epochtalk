b8578235b00faace28bdfdf0e9a6008bb8298908
Anthony Kinsey <anthony@slickage.com>
2017-01-18 13:34:16 -1000

    Fix for recaptcha locking users out.
    * Given 3 free attempts. Exponential timeout starting at 1 min
      for the next 5 attempts. Then "permanently" locked out for 45
      days.
    
    * Attempts now get cleared out once resetting the password
      successfully.

--- server/plugins/backoff/index.js
+++ server/plugins/backoff/index.js
@@ -9,16 +9,14 @@ exports.register = function(plugin, options, next) {
 
     // variables
     var db = request.db.db;
-    var path = request.route.path;
     var remoteAddress = request.info.remoteAddress;
-    var method = request.route.method.toUpperCase();
 
-    // pull records for this ip and method/path
-    return getAccessLogs(db, remoteAddress, path, method)
+    // pull records for this ip
+    return getAccessLogs(db, remoteAddress)
     // calculate backoff pressure
     .then(calculatePressure)
-    // log route (ip, path, method, timestamp)
-    .tap(function() { return appendLog(db, remoteAddress, path, method); })
+    // log route (ip, timestamp)
+    .tap(function() { return appendLog(db, remoteAddress); })
     // reject or allow connection
     .then(function(allowed) {
       if (allowed) { return reply.continue(); }
@@ -27,17 +25,23 @@ exports.register = function(plugin, options, next) {
   });
 
   plugin.expose('release', releasePressure);
+  plugin.expose('getAccessLogs', getAccessLogs);
+  plugin.expose('getLockoutTimes', getLockoutTimes);
 
   next();
 };
 
-function getAccessLogs(db, ip, path, method) {
+function getAccessLogs(db, ip) {
   var q = `
     SELECT ip, route, method, created_at
     FROM backoff
     WHERE ip = $1 AND route = $2 AND method = $3
     ORDER BY created_at DESC LIMIT 10`;
-  return db.sqlQuery(q, [ip, path, method]);
+  return db.sqlQuery(q, [ip, '/api/recover', 'POST']);
+}
+
+function getLockoutTimes() {
+  return [0, 0, 0, 1000*60, 1000*60*2, 1000*60*4, 1000*60*16, 1000*60*256];
 }
 
 function calculatePressure(rows) {
@@ -45,10 +49,10 @@ function calculatePressure(rows) {
   var lastTimestamp, endDate;
   var iterationCount = rows.length;
   if (rows.length) { lastTimestamp = rows[0].created_at; }
-  var times = [0, 0, 1000*60, 1000*60*2, 1000*60*4, 1000*60*16, 1000*60*256];
+  var times = getLockoutTimes();
 
-  if (iterationCount < 2) { endDate = 0; }
-  else if (iterationCount < 7) {
+  if (iterationCount < 3) { endDate = 0; }
+  else if (iterationCount < 8) {
     endDate = lastTimestamp.getTime() + times[iterationCount];
   }
   else {
@@ -59,17 +63,17 @@ function calculatePressure(rows) {
   return allow;
 }
 
-function appendLog(db, ip, path, method) {
+function appendLog(db, ip) {
   var q = `
     INSERT INTO backoff (ip, route, method, created_at)
     VALUES ($1, $2, $3, now());
   `;
-  return db.sqlQuery(q, [ip, path, method]);
+  return db.sqlQuery(q, [ip, '/api/recover', 'POST']);
 }
 
-function releasePressure(db, ip, path, method) {
+function releasePressure(db, ip) {
   var q = `DELETE FROM backoff WHERE ip = $1 AND route = $2 AND method = $3`;
-  return db.sqlQuery(q, [ip, path, method]);
+  return db.sqlQuery(q, [ip, '/api/recover', 'POST']);
 }
 
 
