15d25d59e1820a2c95cdaff0ea66d2feb96751d5
Anthony Kinsey <anthony@slickage.com>
2017-01-24 15:54:09 -1000

    fix for banned roles message in roles management

--- modules/ept-frontend/client/admin/management/roles.controller.js
+++ modules/ept-frontend/client/admin/management/roles.controller.js
@@ -170,9 +170,6 @@ var ctrl = ['$rootScope', '$scope', '$location', 'Session', 'Alert', 'AdminRoles
 
   this.canViewAddUsersControl = function() {
     var view = false;
-    console.log(ctrl.selectedRole.lookup);
-    console.log(Session.user.permissions.priority, ctrl.selectedRole.priority);
-    console.log(ctrl.controlAccess.privilegedAddRoles, ctrl.controlAccess.privilegedAddRoles.samePriority);
     if (ctrl.selectedRole.lookup !== 'banned') {
       if (ctrl.controlAccess.privilegedAddRoles && ctrl.controlAccess.privilegedAddRoles.samePriority) {
         view = ctrl.selectedRole.priority >= Session.user.permissions.priority;
@@ -182,7 +179,6 @@ var ctrl = ['$rootScope', '$scope', '$location', 'Session', 'Alert', 'AdminRoles
       }
     }
     if (ctrl.showFilterUsers) { ctrl.showFilterUsers = view; }
-    console.log(view);
     return view;
   };
 
