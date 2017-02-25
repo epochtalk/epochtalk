1169811101093b225115e26c79e07d4eb6f9ef7b
Anthony Kinsey <anthony@slickage.com>
2017-02-24 16:42:13 -1000

    Convert mentions into links

--- modules/ept-mentions/hooks/index.js
+++ modules/ept-mentions/hooks/index.js
@@ -2,10 +2,33 @@ var Promise = require('bluebird');
 var _ = require('lodash');
 
 var mentionsRegex = /(@[a-zA-Z\d-_.]+)/g;
+var userIdRegex = /<@[^>]+>/g;
 
 function userIdToUsername(request) {
-//  console.log('POST BY THREAD POST', request.pre.processed.posts);
-  return;
+  console.log('POST BY THREAD POST', request.pre.processed.posts);
+
+  return Promise.each(request.pre.processed.posts, post => {
+    var userIds = post.body.match(userIdRegex).map(x => x.substring(2, x.length - 1));
+    return Promise.each(userIds, userId => {
+      // TODO: cache looked up users
+      return request.db.users.find(userId)
+      .then(user => {
+        var idRegex = new RegExp('<@' + userId + '>', 'g');
+
+        // raw_body: <@123> -> @kkid
+        if (post.raw_body) {
+          post.raw_body = post.raw_body.replace(idRegex, '@' + user.username);
+        }
+        else {
+          post.raw_body = post.body.replace(idRegex, '@' + user.username);
+        }
+
+        // body: <@123> -> <a ui-sref=".profiles('kkid')">kkid</a>
+        var profileLink = '<a ui-sref="profile.posts({ username: \'' + user.username + '\'})">' + '@' + user.username + '</a>';
+        post.body = post.body.replace(idRegex, profileLink);
+      });
+    });
+  });
 }
 
 function usernameToUserId(request) {
@@ -20,7 +43,7 @@ function usernameToUserId(request) {
     username = username.substring(1);
     return request.db.users.userByUsername(username)
     .then(function(user) {
-      mentions.push({ replacer: '<' + user.id + '>', replacee: '<@' + user.username.toLowerCase() + '>' });
+      mentions.push({ replacer: '<@' + user.id + '>', replacee: '<@' + user.username.toLowerCase() + '>' });
       return mentions;
     })
     .catch(function() { return mentions; });
@@ -29,25 +52,12 @@ function usernameToUserId(request) {
   .each(function(mention) {
     body = body.replace(new RegExp(mention.replacee, 'g'), mention.replacer);
     rawBody = rawBody.replace(new RegExp(mention.replacee, 'g'), mention.replacer);
-
   })
   .then(function() {
     request.payload.body = body;
     request.payload.raw_body = rawBody;
   });
 }
-
-
-// REGEX MATCH
-// Case 1: '@test' match @{word}
-// Case 2: '@test @test' match all occurrences of @{word}
-// Case 3: '@test@test'
-
-// REGEX REPLACE
-// Case 1: '@test @testawdawd' only replace @{word} not @{word}{othertext}
-// Case 2: 'awdaw@test' only replace @{word} not {othertext}@{word}
-// Case 3: '@test..' replace @{word}{punctuation}
-// Case 4: '@test@test' replace @{word}@{word}
 
 module.exports = [
   { path: 'posts.byThread.post', method: userIdToUsername },
