67118f7a335ee5a8b56b4b74275298e05750aa4c
Bronson Oka <boka@slickage.com>
2017-03-13 17:45:03 -1000

    add test/db
    preloads db dependencies for use in tests

---
+++ test/db.js
@@ -0,0 +1,5 @@
+var path = require('path');
+module.exports = {
+  categories: require(path.normalize(__dirname + '/../modules/ept-categories')).db,
+  boards: require(path.normalize(__dirname + '/../modules/ept-boards')).db
+};
