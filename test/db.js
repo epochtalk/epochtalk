144823771dfe6b8ac00d7abeec170e9629aa15be
Bronson Oka <boka@slickage.com>
2017-03-16 13:24:40 -1000

    test: use TEST_DATABASE_URL in test/db.js

--- test/db.js
+++ test/db.js
@@ -1,4 +1,5 @@
 require('dotenv').load();
+process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
 var path = require('path');
 module.exports = {
   categories: require(path.normalize(__dirname + '/../modules/ept-categories')).db,
