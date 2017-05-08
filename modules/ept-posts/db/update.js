dedf6c63f39d94f435efee6d3d4ff080ccf60af4
James Wang <jw@slickage.com>
2017-05-07 22:52:54 -1000

    Breadcrumb query for post title fixed

--- modules/ept-posts/db/update.js
+++ modules/ept-posts/db/update.js
@@ -10,7 +10,7 @@ module.exports = function(post) {
   post = helper.deslugify(post);
   return using(db.createTransaction(), function(client) {
     var q, params;
-    q = 'SELECT title, body, raw_body FROM posts WHERE id = $1 FOR UPDATE';
+    q = 'SELECT content ->> \'title\' as title, content ->> \'body\' as body FROM posts WHERE id = $1 FOR UPDATE';
     return client.queryAsync(q, [post.id])
     .then(function(results) {
       if (results.rows.length > 0) { return results.rows[0]; }
@@ -19,12 +19,13 @@ module.exports = function(post) {
     .then(function(oldPost) {
       post.title = post.title || oldPost.title;
       helper.updateAssign(post, oldPost, post, 'body');
-      helper.updateAssign(post, oldPost, post, 'raw_body');
       post.thread_id = post.thread_id || oldPost.thread_id;
     })
     .then(function() {
-      q = 'UPDATE posts SET title = $1, body = $2, raw_body = $3, thread_id = $4, updated_at = now() WHERE id = $5 RETURNING updated_at';
-      params = [post.title, post.body, post.raw_body, post.thread_id, post.id];
+      console.log("POST");
+      console.log(post);
+      q = 'UPDATE posts SET content = content || \'{"title": $1, "body": $2}\', thread_id = $3, updated_at = now() WHERE id = $4 RETURNING updated_at';
+      params = [post.title, post.body, post.thread_id, post.id];
       return client.queryAsync(q, params)
       .then(function(results) { post.updated_at = results.rows[0].updated_at; });
     });
