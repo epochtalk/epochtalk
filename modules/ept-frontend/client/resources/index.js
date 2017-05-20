e9c0835c5e4b4b8e5c98e768b967c40b1d418bc2
Anthony Kinsey <anthony@slickage.com>
2017-05-19 14:23:17 -1000

    Move resources for ept-reports into ept-reports

--- modules/ept-frontend/client/resources/index.js
+++ modules/ept-frontend/client/resources/index.js
@@ -2,7 +2,6 @@ require('./admin');
 
 angular.module('ept')
   .factory('Breadcrumbs', require('./breadcrumbs.js'))
-  .factory('Reports', require('./reports.js'))
   .factory('Messages', require('./messages.js'))
   .factory('Conversations', require('./conversations.js'))
   .factory('Bans', require('./bans.js'))
