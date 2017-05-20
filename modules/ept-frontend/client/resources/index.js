6001ddfc5d20f7457312b38ea5f547a338aaf827
Anthony Kinsey <anthony@slickage.com>
2017-05-19 14:32:52 -1000

    Moved resources for ept-messages into ept-messages

--- modules/ept-frontend/client/resources/index.js
+++ modules/ept-frontend/client/resources/index.js
@@ -2,8 +2,6 @@ require('./admin');
 
 angular.module('ept')
   .factory('Breadcrumbs', require('./breadcrumbs.js'))
-  .factory('Messages', require('./messages.js'))
-  .factory('Conversations', require('./conversations.js'))
   .factory('Bans', require('./bans.js'))
   .factory('UserNotes', require('./user-notes.js'))
   .factory('Notifications', require('./notifications.js'))
