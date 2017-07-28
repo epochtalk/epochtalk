0ae92b462e9ccdbc7c05a203f795928bd87bff9e
Anthony Kinsey <anthony@slickage.com>
2017-07-27 15:26:26 -1000

    Remove useless reports statuses table and replaced with enum
    * status is now stored directly on report using report_status_type enum

--- modules/ept-reports/db/messages/reports/create.js
+++ modules/ept-reports/db/messages/reports/create.js
@@ -5,20 +5,11 @@ var db = dbc.db;
 var helper = dbc.helper;
 var using = Promise.using;
 
-//  Report Statuses
-//  id | priority |   status
-// ----+----------+------------
-//   1 |        1 | Pending
-//   2 |        2 | Reviewed
-//   3 |        3 | Ignored
-//   4 |        4 | Bad Report
-
 module.exports = function(messageReport) {
   messageReport = helper.deslugify(messageReport);
   return using(db.createTransaction(), function(client) {
-    var q = 'INSERT INTO administration.reports_messages(status_id, reporter_user_id, reporter_reason, offender_message_id, created_at, updated_at) VALUES($1, $2, $3, $4, now(), now()) RETURNING id';
-    var statusId = 1; // New reports are always pending
-    var params = [statusId, messageReport.reporter_user_id, messageReport.reporter_reason, messageReport.offender_message_id];
+    var q = 'INSERT INTO administration.reports_messages(reporter_user_id, reporter_reason, offender_message_id, created_at, updated_at) VALUES($1, $2, $3, now(), now()) RETURNING id';
+    var params = [messageReport.reporter_user_id, messageReport.reporter_reason, messageReport.offender_message_id];
     return client.queryAsync(q, params)
     .then(function(results) { // return created report id
       var rows = results.rows;
@@ -26,7 +17,7 @@ module.exports = function(messageReport) {
       else { return Promise.reject(); }
     })
     .then(function(reportId) { // Lookup the created report and return it
-      q = 'SELECT ru.id, rs.status, ru.reporter_user_id, ru.reporter_reason, ru.reviewer_user_id, ru.offender_message_id, ru.created_at, ru.updated_at FROM administration.reports_messages ru JOIN administration.reports_statuses rs ON(ru.status_id = rs.id) WHERE ru.id = $1';
+      q = 'SELECT ru.id, ru.status, ru.reporter_user_id, ru.reporter_reason, ru.reviewer_user_id, ru.offender_message_id, ru.created_at, ru.updated_at FROM administration.reports_messages ru WHERE ru.id = $1';
       params = [reportId];
       return client.queryAsync(q, params);
     })
--- modules/ept-reports/db/posts/reports/create.js
+++ modules/ept-reports/db/posts/reports/create.js
@@ -5,20 +5,11 @@ var db = dbc.db;
 var helper = dbc.helper;
 var using = Promise.using;
 
-//  Report Statuses
-//  id | priority |   status
-// ----+----------+------------
-//   1 |        1 | Pending
-//   2 |        2 | Reviewed
-//   3 |        3 | Ignored
-//   4 |        4 | Bad Report
-
 module.exports = function(postReport) {
   postReport = helper.deslugify(postReport);
   return using(db.createTransaction(), function(client) {
-    var q = 'INSERT INTO administration.reports_posts(status_id, reporter_user_id, reporter_reason, offender_post_id, created_at, updated_at) VALUES($1, $2, $3, $4, now(), now()) RETURNING id';
-    var statusId = 1; // New reports are always pending
-    var params = [statusId, postReport.reporter_user_id, postReport.reporter_reason, postReport.offender_post_id];
+    var q = 'INSERT INTO administration.reports_posts(reporter_user_id, reporter_reason, offender_post_id, created_at, updated_at) VALUES($1, $2, $3, now(), now()) RETURNING id';
+    var params = [postReport.reporter_user_id, postReport.reporter_reason, postReport.offender_post_id];
     return client.queryAsync(q, params)
     .then(function(results) { // return created report id
       var rows = results.rows;
@@ -26,7 +17,7 @@ module.exports = function(postReport) {
       else { return Promise.reject(); }
     })
     .then(function(reportId) { // Lookup the created report and return it
-      q = 'SELECT rp.id, rs.status, rp.reporter_user_id, rp.reporter_reason, rp.reviewer_user_id, rp.offender_post_id, rp.created_at, rp.updated_at FROM administration.reports_posts rp JOIN administration.reports_statuses rs ON(rp.status_id = rs.id) WHERE rp.id = $1';
+      q = 'SELECT rp.id, rp.status, rp.reporter_user_id, rp.reporter_reason, rp.reviewer_user_id, rp.offender_post_id, rp.created_at, rp.updated_at FROM administration.reports_posts rp WHERE rp.id = $1';
       params = [reportId];
       return client.queryAsync(q, params);
     })
