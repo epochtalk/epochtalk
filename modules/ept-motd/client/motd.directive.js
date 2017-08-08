0aa6b3ed59ae651890b1a451451c0b2ef437a452
Anthony Kinsey <anthonyjk@gmail.com>
2017-08-08 13:07:26 -1000

    Added Announcement preview and main view only option is now functional
    * Preview in admin panel when typing Announcement
    
    * Main View Only option now works

--- modules/ept-motd/client/motd.directive.js
+++ modules/ept-motd/client/motd.directive.js
@@ -1,4 +1,4 @@
-var html = `<div ng-if="vmMotd.data.motd_html.length" id="motd-wrap"><div id="motd" post-processing="vmMotd.data.motd_html" style-fix="true"></div></div>`;
+var html = `<div ng-if="vmMotd.data.motd_html.length && !vmMotd.hideAnnnouncement" id="motd-wrap"><div id="motd" post-processing="vmMotd.data.motd_html" style-fix="true"></div></div>`;
 
 var directive = ['Motd', function(Motd) {
   return {
@@ -6,14 +6,30 @@ var directive = ['Motd', function(Motd) {
     template: html,
     scope: true,
     controllerAs: 'vmMotd',
-    controller: [function() {
+    controller: ['$rootScope', '$state', function($rootScope, $state) {
       var ctrl = this;
 
       this.data = '';
+      this.hideAnnnouncement = false;
+
+      $rootScope.$on('$locationChangeSuccess', function() {
+        mainViewOnlyCheck();
+      });
+
+      function mainViewOnlyCheck() {
+        if (ctrl.data.main_view_only) {
+          var curState = $state.current.name;
+          // Only show on main views
+          if (curState !== 'boards' && curState !== 'home' && curState !== 'portal') {
+            ctrl.hideAnnnouncement = true;
+          }
+        }
+      }
 
       Motd.get().$promise
       .then(function(data) {
         ctrl.data = data;
+        mainViewOnlyCheck();
       })
       .catch(function(err) {
         console.log(err);
