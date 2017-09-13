6855a489e88ba1b0bcfd1aa3a87f0d918aa5b442
Anthony Kinsey <anthony@slickage.com>
2017-09-12 16:23:59 -1000

    Fix for focusing on textfield when modal opens

--- modules/ept-frontend/client/components/index.js
+++ modules/ept-frontend/client/components/index.js
@@ -8,6 +8,7 @@ app.directive('uniqueEmail', require('./unique_email/unique-email.directive.js')
 app.directive('imageLoader', require('./image_loader/image_loader.directive.js'));
 app.directive('modal', require('./modal/modal.directive.js'));
 app.directive('autoFocus', require('./autofocus/autofocus.directive.js'));
+app.directive('modalFocus', require('./modal_focus/modal-focus.directive.js'));
 app.directive('postProcessing', require('./post_processing/post-processing.directive.js'));
 app.directive('invite', require('./invite/invite.directive.js'));
 
