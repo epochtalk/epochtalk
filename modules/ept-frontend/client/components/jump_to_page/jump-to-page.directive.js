738b9d00af64edd18726554925128b4497d4e09c
Anthony Kinsey <anthony@slickage.com>
2017-09-28 17:08:08 -1000

    Polishing jump to page feature

--- modules/ept-frontend/client/components/jump_to_page/jump-to-page.directive.js
+++ modules/ept-frontend/client/components/jump_to_page/jump-to-page.directive.js
@@ -3,12 +3,12 @@ module.exports = ['$location', '$state', function($location, $state) {
     restrict: 'E',
     replace: true,
     transclude: true,
-    scope: { pageCount: '=' },
+    scope: { pageCount: '=', focus: '=', clickAction: '=' },
     template:
     `<div class="jump-to-page">
-      <input ng-model="pageNum" type="number" />
-      <button ng-click="goToPage()">Go</button>
-    </div>`,
+       <input ng-model="pageNum" auto-focus="focus" select="true" type="number" ng-keyup="$event.keyCode === 13 && goToPage()" />
+       <button ng-click="goToPage()">Go</button>
+     </div>`,
     link: function(scope) {
       scope.pageNum = Number($location.search().page) || 1;
 
@@ -16,14 +16,19 @@ module.exports = ['$location', '$state', function($location, $state) {
         // default to last page
         var page = scope.pageNum;
 
+        console.log(page, scope.pageCount);
+
         if (page > scope.pageCount) {
           page = scope.pageCount;
         }
         else if (scope.pageNum < 1) {
           page = 1;
         }
+        console.log(page);
+
 
         $state.go($state.current, { page: page });
+        if (scope.clickAction) { scope.clickAction(); }
       };
 
       scope.$watch(function() { return $location.search().page; }, function(newPage) {
