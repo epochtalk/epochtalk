c8dab9ba60a997e69196bc7da9b584a310f011ee
Anthony Kinsey <anthony@slickage.com>
2017-10-17 14:02:42 -1000

    Added route documentation for thread subscriptions

--- modules/ept-thread-notifications/routes/index.js
+++ modules/ept-thread-notifications/routes/index.js
@@ -1,5 +1,17 @@
 var Joi = require('joi');
 
+/**
+  * @apiVersion 0.4.0
+  * @apiGroup ThreadNotifications
+  * @api {GET} /threadnotifications Get Thread Notification Settings
+  * @apiName GetThreadNotificationSettings
+  * @apiPermission User
+  * @apiDescription Used to retreive the user's thread notification settings
+  *
+  * @apiSuccess {boolean} notify_replied_threads Boolean indicating if the user is receiving thread notifications
+  *
+  * @apiError (Error 500) InternalServerError There was an getting thread notification settings
+  */
 var getNotificationSettings = {
   method: 'GET',
   path: '/api/threadnotifications',
@@ -17,6 +29,21 @@ var getNotificationSettings = {
   }
 };
 
+
+/**
+  * @apiVersion 0.4.0
+  * @apiGroup ThreadNotifications
+  * @api {PUT} /threadnotifications Toggle Thread Notifications
+  * @apiName ToggleThreadNotifications
+  * @apiPermission User
+  * @apiDescription Used to toggle thread notifications
+  *
+  * @apiParam (Payload) {boolean} [enabled=true] Boolean indicating if thread notifications are enabled or not
+  *
+  * @apiSuccess {boolean} enabled Boolean indicating if the thread notifications were enabled or not
+  *
+  * @apiError (Error 500) InternalServerError There was an enabling thread notifications
+  */
 var enableNotifications = {
   method: 'PUT',
   path: '/api/threadnotifications',
@@ -36,6 +63,18 @@ var enableNotifications = {
   }
 };
 
+/**
+  * @apiVersion 0.4.0
+  * @apiGroup Mentions
+  * @api {DELETE} /threadnotifications Remove Thread Subscriptions
+  * @apiName RemoveThreadSubscriptions
+  * @apiPermission User
+  * @apiDescription Used to delete a user's thread subscriptions
+  *
+  * @apiSuccess {boolean} deleted True if the user's thread subscriptions were deleted
+  *
+  * @apiError (Error 500) InternalServerError There was an issue deleting the user's thread subscriptions
+  */
 var removeSubscriptions = {
   method: 'DELETE',
   path: '/api/threadnotifications',
