72e75fb80a5b3d52cc5caae59c4e2eb47d39b38c
Anthony Kinsey <anthonyjk@gmail.com>
2017-10-31 12:16:45 -1000

    Fix for portal view not parsing bbcode and mentions not working
    * bbcode wasn't being parsed on the portal view
    
    * fixed issue with mentions rendering as userids in portal view
    
    * fixes #383

--- modules/ept-portal/index.js
+++ modules/ept-portal/index.js
@@ -3,11 +3,13 @@ var db = require(path.normalize(__dirname + '/db'));
 var routes = require(path.normalize(__dirname + '/routes'));
 var permissions = require(path.normalize(__dirname + '/permissions'));
 var authorization = require(path.normalize(__dirname + '/authorization'));
+var common = require(path.normalize(__dirname + '/common'));
 
 module.exports =  {
   name: 'portal',
   permissions: permissions,
   db: db,
   routes: routes,
+  common: common.export(),
   authorization: authorization
 };
