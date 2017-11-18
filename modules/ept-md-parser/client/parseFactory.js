bb0ec19121b4d7961a898c91b8d1905a89b4e01f
Anthony Kinsey <anthony@slickage.com>
2017-11-17 14:59:20 -1000

    Fix for md parser ignoring newlines

--- modules/ept-md-parser/client/parseFactory.js
+++ modules/ept-md-parser/client/parseFactory.js
@@ -3,13 +3,18 @@ var renderer;
 
 function parse(input) {
   if (!input) { input = ''; }
-
   // escape double forward slashes before parsing markdown
   input = input.replace(/\/\//g, '&#47;&#47;');
+
+  // manually handle newlines
+  input = input.replace(/\n/g, String.fromCharCode(26));
+
   // compile markdown
   input = compiler(input, { renderer: renderer });
+
   // preserve whitespacing
   input = input.replace(/\r\n|\r|\n/g, String.fromCharCode(26));
+
   // replace whitespacing
   input = input.replace(new RegExp(String.fromCharCode(26), 'g'), '\n');
 
