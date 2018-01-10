c8943f0e4f852088ea30ae1e1803e3ee824bafd6
Anthony Kinsey <anthony@slickage.com>
2018-01-09 16:01:35 -1000

    Add more test cases for color validator

--- test/protractor/color-validator.spec.js
+++ test/protractor/color-validator.spec.js
@@ -10,16 +10,67 @@ describe('Directive: Color Validator', function() {
     browser.get(helper.host + '/admin/settings/theme');
   });
 
-  it('"invalid-color" should be an invalid color', function() {
+  // Negative test cases
+  it('A number should be an invalid color', function() {
+    color.clear();
+    color.sendKeys(1);
+    expect(helper.hasClass(color, 'ng-invalid')).toBe(true);
+    expect(helper.hasClass(color, 'ng-dirty')).toBe(true);
+  });
+
+  it('A non-color string should be an invalid color', function() {
     color.clear();
     color.sendKeys('invalid-color');
     expect(helper.hasClass(color, 'ng-invalid')).toBe(true);
     expect(helper.hasClass(color, 'ng-dirty')).toBe(true);
   });
 
-  it('"#ccc" should be a valid color', function() {
+  // Positive test cases
+  it('A color string should be a valid color', function() {
     color.clear();
-    color.sendKeys('#ccc');
+    color.sendKeys('white');
+    expect(helper.hasClass(color, 'ng-valid')).toBe(true);
+    expect(helper.hasClass(color, 'ng-dirty')).toBe(true);
+  });
+
+  it('Shorthand hexadecimal values should be a valid color', function() {
+    color.clear();
+    color.sendKeys('#fff');
+    expect(helper.hasClass(color, 'ng-valid')).toBe(true);
+    expect(helper.hasClass(color, 'ng-dirty')).toBe(true);
+  });
+
+  it('Hexadecimal values should be a valid color', function() {
+    color.clear();
+    color.sendKeys('#ffffff');
+    expect(helper.hasClass(color, 'ng-valid')).toBe(true);
+    expect(helper.hasClass(color, 'ng-dirty')).toBe(true);
+  });
+
+  it('RGB values should be a valid color', function() {
+    color.clear();
+    color.sendKeys('rgb(255,255,255)');
+    expect(helper.hasClass(color, 'ng-valid')).toBe(true);
+    expect(helper.hasClass(color, 'ng-dirty')).toBe(true);
+  });
+
+  it('RGBA values should be a valid color', function() {
+    color.clear();
+    color.sendKeys('rgba(255,255,255,0.5)');
+    expect(helper.hasClass(color, 'ng-valid')).toBe(true);
+    expect(helper.hasClass(color, 'ng-dirty')).toBe(true);
+  });
+
+  it('HSL values should be a valid color', function() {
+    color.clear();
+    color.sendKeys('hsl(120, 100%, 50%)');
+    expect(helper.hasClass(color, 'ng-valid')).toBe(true);
+    expect(helper.hasClass(color, 'ng-dirty')).toBe(true);
+  });
+
+  it('HSLA values should be a valid color', function() {
+    color.clear();
+    color.sendKeys('hsla(120, 100%, 50%, 0.5)');
     expect(helper.hasClass(color, 'ng-valid')).toBe(true);
     expect(helper.hasClass(color, 'ng-dirty')).toBe(true);
   });
