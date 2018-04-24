24b43e2f8eb4491f17bfc6b8f8de2a6b8bb73644
Anthony Kinsey <anthony@slickage.com>
2018-04-24 11:16:42 -1000

    Move rank data into a metadata property
    * Storing the rank info within the metadata property so the data
      doesn't dirty up the original byThreads request data.

--- modules/ept-rank/hooks/index.js
+++ modules/ept-rank/hooks/index.js
@@ -1,12 +1,11 @@
 function getRankData(request) {
   return request.db.rank.getMaps()
   .then(function(maps) {
-    request.pre.processed.rank_metric_maps = maps;
+    request.pre.processed.metadata = {};
+    request.pre.processed.metadata.rank_metric_maps = maps;
     return request.db.rank.getRanks();
   })
-  .then(function(ranks) {
-    request.pre.processed.ranks = ranks;
-  });
+  .then(function(ranks) { request.pre.processed.metadata.ranks = ranks; });
 }
 
 module.exports = [
