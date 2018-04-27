d9592afbf6c8ebe8c572c41cf4497410e61829d0
Anthony Kinsey <anthonyjk@gmail.com>
2018-04-26 14:52:59 -1000

    Implement Admin UI Add Rank
    * Implemented Add interface for ranks
    
    * WIP, still implementing UI for edit and delete

--- modules/ept-rank/client/rank-manager.directive.js
+++ modules/ept-rank/client/rank-manager.directive.js
@@ -1,17 +1,43 @@
+var cloneDeep = require('lodash/cloneDeep');
+
 var directive = ['Ranks', 'Alert', '$timeout', function(Ranks, Alert, $timeout) {
   return {
     restrict: 'E',
     scope: true,
-    bindToController: { },
     template: require('./rank-manager.directive.html'),
     controllerAs: 'vm',
     controller: ['$scope', function($scope) {
       var ctrl = this;
+      this.newRank = {};
 
       Ranks.get().$promise
       .then(function(ranks) {
         ctrl.ranks = ranks;
       });
+
+
+      // Add Rank Modal
+      this.saveRankBtnLabel = 'Save Rank';
+      this.closeAdd = function() { ctrl.newRank = {}; }
+
+      this.addRank = function() {
+        var updatedRanks = cloneDeep(ctrl.ranks);
+        updatedRanks.push(ctrl.newRank);
+        Ranks.upsert(updatedRanks).$promise
+        .then(function(latestRanks) {
+          ctrl.ranks = latestRanks;
+          Alert.success('Sucessfully added rank: ' + ctrl.newRank.name);
+          ctrl.newRank = {};
+          ctrl.showAddModal = false;
+        })
+        .catch(function(err) {
+          var errMsg = 'There was an error adding the rank';
+          if (err && err.data && err.data.statusCode === 400) {
+            errMsg = 'Error: Ranks must have unique post counts'
+          }
+          Alert.error(errMsg);
+        });
+      }
 
     }]
   };
