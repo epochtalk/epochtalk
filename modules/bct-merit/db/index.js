81fd31d7876ebf7ab265b7693da518118dd84e6c
Bronson Oka <boka@Bronsons-MacBook.local>
2018-05-08 11:29:28 -1000

    clarify merit recalculation algorithm and document

--- modules/bct-merit/db/index.js
+++ modules/bct-merit/db/index.js
@@ -4,20 +4,26 @@ var dbc = require(path.normalize(__dirname + '/db'));
 var db = dbc.db;
 var using = Promise.using;
 
+// calculate user's merit
+// update merit for user
+// return merit
 function recalculateMerit(userId) {
   var queryMerit = 'SELECT SUM(amount) AS merit FROM merit_ledger WHERE to_user_id = $1';
   var updateMerit = 'UPDATE merit_users SET merit = $1 WHERE user_id = $2';
-  var merit = 0;
 
   return using(db.createTransaction(), function(client) {
+    // query sum of merit transactions from merit ledger
     return client.query(queryMerit, [userId])
     .then(function(results) {
-      if (results.rows.length) {
-        merit = results.rows[0].merit;
-        return client.query(updateMerit, [merit, userId])
-        .then(function() { return merit; });
+      // if there is a row returned
+      // and there is a sum of merit transactions
+      if (results.rows.length && results.rows[0].merit) {
+        var merit = results.rows[0].merit;
+        // update user's merit to the sum of merit from transactions, returning merit
+        return client.query(updateMerit, [merit, userId]).then(function() { return merit; });
       }
-      else { return merit; }
+      // otherwise, don't update the user's merit, return zero merit
+      else { return 0; }
     });
   });
 }
