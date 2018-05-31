67a6a3ed3c68089262e98009fcfea35a94bf5beb
Anthony Kinsey <anthony@slickage.com>
2018-05-31 11:49:24 -1000

    Add validation to send merit button
    * Only show send merit button if a user is not the post owner
      and the user is authenticated

--- modules/bct-merit/client/send-merit-button.directive.js
+++ modules/bct-merit/client/send-merit-button.directive.js
@@ -1,15 +1,21 @@
-var html = '<li>' +
+var html = '<li ng-if="vmSMB.notOwner && vmSMB.loggedIn()">' +
            '  <a ng-href="#" data-balloon="Merit Post" ng-click="PostsParentCtrl.meritPost = post; PostsParentCtrl.showMeritModal = true;">' +
            '    <i class="fa fa-plus-square"></i>' +
            '  </a>' +
            '</li>';
 
-var directive = [function() {
+var directive = ['Session', function(Session) {
   return {
     restrict: 'E',
     scope: true,
     replace: true,
-    template: html
+    template: html,
+    bindToController: { post: '=' },
+    controllerAs: 'vmSMB',
+    controller: [function() {
+      this.loggedIn = Session.isAuthenticated;
+      this.notOwner = Session.user.id !== this.post.user.id;
+    }]
   };
 }];
 
