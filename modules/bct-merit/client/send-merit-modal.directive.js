5c52c41df112da6b39b9279676b924b362eba0ef
Anthony Kinsey <anthony@slickage.com>
2018-05-31 12:52:27 -1000

    Implement send merit modal
    * Modal allows users to send merit to one another
    
    * Once merit is successfully sent, the merit line on the post should
      appear and show by whom and how much merit was sent to the post

--- modules/bct-merit/client/send-merit-modal.directive.js
+++ modules/bct-merit/client/send-merit-modal.directive.js
@@ -1,29 +1,59 @@
-var html = '<modal show="PostsParentCtrl.showMeritModal">' +
-           '  <button ng-click="vmSMM.sendMerit()">{{vmSMM.post.id}}</button>' +
-           '</modal>';
-
-var directive = ['Merit', 'Alert', function(Merit, Alert) {
+var directive = ['Merit', 'Alert', 'Session', function(Merit, Alert, Session) {
   return {
     restrict: 'E',
     scope: true,
-    bindToController: { post: '=' },
-    template: html,
+    bindToController: { post: '=', allPosts: '=', showMeritModal: '=' },
+    template: require('./send-merit-modal.html'),
     controllerAs: 'vmSMM',
     controller: ['$scope', function($scope) {
       var ctrl = this;
+      this.authUser = Session.user;
+
+      this.meritAmount = 1;
+      this.sendMeritBtnLabel = 'Send Merit';
+
       this.sendMerit = function() {
-        return Merit.send({ to_user_id: ctrl.post.user.id, post_id: ctrl.post.id, amount: 1 }).$promise
-        .then(function(result) {
-          Alert.success(result);
+        ctrl.sendMeritBtnLabel = 'Loading...';
+        ctrl.sendMeritSubmitted = true;
+        return Merit.send({ to_user_id: ctrl.post.user.id, post_id: ctrl.post.id, amount: ctrl.meritAmount }).$promise
+        .then(function() {
+          // update the posts merit array, updates merited by UI
+          var found = false;
+          for (var i = 0; i < ctrl.post.merits.length; i++) {
+            var merit = ctrl.post.merits[i];
+            if (merit.username === ctrl.authUser.username) {
+              merit.amount = Number(merit.amount) + Number(ctrl.meritAmount);
+              found = true;
+              break;
+            }
+          }
+          if (!found) {
+            ctrl.post.merits.push({ username: ctrl.authUser.username, amount: ctrl.meritAmount });
+          }
+
+          // update the user's personal merit in all visible posts in the thread, updates post authors merit in the UI
+          ctrl.allPosts.forEach(function(post) {
+            if (post.user.id === ctrl.post.user.id) {
+              post.user.merit = Number(post.user.merit) + Number(ctrl.meritAmount);
+            }
+          });
+
+          Alert.success('Sucessfully sent ' + ctrl.meritAmount + ' merit to ' + ctrl.post.user.username);
         })
         .catch(function(e){
-          console.log(e);
-          Alert.error(e);
+          var errMsg = 'There was an error meriting the post.';
+          if (e.data && e.data.message) { errMsg = e.data.message; }
+          Alert.error(errMsg);
+        })
+        .finally(function() {
+          ctrl.sendMeritBtnLabel = 'Send Merit';
+          ctrl.sendMeritSubmitted = false;
+          ctrl.showMeritModal = false;
+          ctrl.meritAmount = 1;
         });
       };
     }]
   };
 }];
-
 
 angular.module('ept').directive('sendMeritModal', directive);
