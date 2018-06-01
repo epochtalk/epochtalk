72fa84c40b53e3ab746e72b3229e10ad404e44f4
Anthony Kinsey <anthony@slickage.com>
2018-05-31 16:02:20 -1000

    Show link to view merit stats overview

--- modules/bct-merit/client/merit-display.directive.js
+++ modules/bct-merit/client/merit-display.directive.js
@@ -1,12 +1,13 @@
-var html = '<div ng-if="vm.userMerit > -1" title="Merit: {{vm.userMerit}}">Merit: {{vm.userMerit}}</div>';
+var html = '<div ng-if="vmM.userMerit > -1" title="Merit: {{vmM.userMerit}}">Merit: {{vmM.userMerit}}</div>' +
+           '<a ng-if="vmM.username" ui-sref="merit({ username: vmM.username })">View Merit Statistics</a>';
 
 var directive = [function() {
   return {
     restrict: 'E',
     scope: true,
-    bindToController: { merit: '='},
+    bindToController: { merit: '=', username: '=' },
     template: html,
-    controllerAs: 'vm',
+    controllerAs: 'vmM',
     controller: ['$scope', function($scope) {
       var ctrl = this;
       $scope.$watch(function() { return ctrl.merit; }, function(merit) {
