a8f63f50b9ab781f60358f9209a12af2549e311c
Bronson Oka <boka@slickage.com>
2018-06-13 13:51:03 -1000

    change all spend -> send

--- lol.js
+++ lol.js
@@ -1,5 +1,5 @@
 var Promise = require('bluebird');
-var spends = [
+var sends = [
   // before source
   { time: 0, amount: 11 },
   { time: 1, amount: 15 },
@@ -11,13 +11,13 @@ var spends = [
   { time: 5, amount: 4 },
   { time: 6, amount: 57 },
   { time: 7, amount: 9 },
-  // single spend in time window
+  // single send in time window
   { time: 10, amount: 11 },
-  // ff all, then multiple spends per time window
+  // ff all, then multiple sends per time window
   { time: 20, amount: 9 },
   { time: 22, amount: 11 },
   { time: 23, amount: 12 },
-  // ff all, then multiple spends per time window
+  // ff all, then multiple sends per time window
   { time: 40, amount: 1 },
   { time: 42, amount: 13 },
   { time: 43, amount: 1 },
@@ -55,11 +55,11 @@ var range = [];
 var rangeSum = 0;
 var sourcePos = 0;
 
-Promise.each(spends, function(spend) {
+Promise.each(sends, function(send) {
   // if there is a next source amount
   // and
-  // the current spend's time is at or after the next source amount
-  if (sourcePos < sources.length - 1 && spend.time >= sources[sourcePos+1].time) {
+  // the current send's time is at or after the next source amount
+  if (sourcePos < sources.length - 1 && send.time >= sources[sourcePos+1].time) {
     console.log('RESET FOR SOURCE');
     // update the source pos
     sourcePos++;
@@ -69,28 +69,28 @@ Promise.each(spends, function(spend) {
     range = [];
   }
   // clear the range until it's within the window
-  while (range.length > 0 && (spend.time - range[0].time > timeWindow)) {
-    console.log('FF RANGE; TIMEDIFF:', spend.time - range[0].time, 'rangelength:', range.length);
+  while (range.length > 0 && (send.time - range[0].time > timeWindow)) {
+    console.log('FF RANGE; TIMEDIFF:', send.time - range[0].time, 'rangelength:', range.length);
     // remove the entry and subtract from sum
     var entry = range.shift();
     rangeSum -= entry.amount;
   }
   // accumulate excess sent
   // remaining source = sources[sourcePos].amount - range sum
-  // don't double count spends
+  // don't double count sends
   var remainingSource = Math.max(sources[sourcePos].amount - rangeSum, 0);
-  excessSent += Math.max(spend.amount - remainingSource, 0);
+  excessSent += Math.max(send.amount - remainingSource, 0);
   console.log('source for range:', sources[sourcePos]);
   console.log('range:');
   console.log('  sum:', rangeSum);
   console.log('  array:', range);
   console.log('remainingSrc:', remainingSource);
-  console.log('spend:', spend);
-  console.log('current excess sent:', Math.max(spend.amount - remainingSource, 0));
+  console.log('send:', send);
+  console.log('current excess sent:', Math.max(send.amount - remainingSource, 0));
   console.log('new total excess sent:', excessSent);
   // add to sum and insert the entry at the end
-  rangeSum += spend.amount;
-  range.push(spend);
+  rangeSum += send.amount;
+  range.push(send);
   console.log();
 })
 .then(function(originalArray) {
