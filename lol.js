f21e7a4c05b2ed8b872c1d008d7903670f427f0e
Bronson Oka <boka@slickage.com>
2018-06-12 16:41:12 -1000

    spends/allocates cover more cases

--- lol.js
+++ lol.js
@@ -1,24 +1,41 @@
 var Promise = require('bluebird');
 var spends = [
+  // before source
   { time: 0, amount: 11 },
   { time: 1, amount: 15 },
   { time: 2, amount: 9 },
+  // use all of source at once
+  // not exceeding window
   { time: 3, amount: 11 },
   { time: 4, amount: 1 },
   { time: 5, amount: 4 },
   { time: 6, amount: 57 },
   { time: 7, amount: 9 },
+  // single spend in time window
   { time: 10, amount: 11 },
+  // ff all, then multiple spends per time window
   { time: 20, amount: 9 },
   { time: 22, amount: 11 },
+  { time: 23, amount: 12 },
+  // ff all, then multiple spends per time window
   { time: 40, amount: 1 },
-  { time: 42, amount: 1 },
-  { time: 50, amount: 4 },
-  { time: 52, amount: 9 },
-  { time: 53, amount: 11 },
-  { time: 54, amount: 1 },
-  { time: 59, amount: 4 },
-  { time: 60, amount: 4 }
+  { time: 42, amount: 13 },
+  { time: 43, amount: 1 },
+  { time: 44, amount: 1 },
+  { time: 45, amount: 1 },
+  { time: 46, amount: 1 }, // should ff here
+  { time: 47, amount: 1 },
+  { time: 48, amount: 1 }, // should ff here
+  { time: 49, amount: 1 }, // should ff here
+  { time: 50, amount: 4 }, // should ff here
+  { time: 52, amount: 9 }, // should ffx2 here
+  { time: 53, amount: 11 }, // should ff here
+  { time: 54, amount: 1 }, // should ff here
+  { time: 55, amount: 1 }, // should ff here
+  { time: 56, amount: 1 }, // should ff here
+  { time: 59, amount: 4 }, // should ffx2 here
+  { time: 60, amount: 4 },
+  { time: 61, amount: 4 }
 ];
 // limits per time window, with allocation time
 var sources = [
@@ -27,7 +44,7 @@ var sources = [
   { time: 10, amount: 15 },
   // { time: 25, amount: 9 },
   // { time: 30, amount: 11 },
-  { time: 50, amount: 4 }
+  { time: 60, amount: 4 }
 ];
 var timeWindow = 5;
 // total excess
