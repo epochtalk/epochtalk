6a44186b6f0c25287010ae512aa865667ed96437
James Wang <james@slickage.com>
2018-10-10 16:38:40 -1000

    webpack config into scripts/webpack.js
    - deprecated keys updated and fixed compilation
    - extra promise layer removed

--- scripts/serve.js
+++ scripts/serve.js
@@ -2,7 +2,7 @@ require('dotenv').load({silent: true});
 var path = require('path');
 var sass = require(path.join(__dirname, 'tasks', 'sass'));
 var clean = require(path.join(__dirname, 'tasks', 'clean'));
-var pack = require(path.join(__dirname, 'tasks', 'webpack'));
+var webpack = require(path.join(__dirname, 'tasks', 'webpack'));
 var symlink = require(path.join(__dirname, 'tasks', 'symlink'));
 var copy_css = require(path.join(__dirname, 'tasks', 'copy_files'));
 var watchFiles = require(path.join(__dirname, 'tasks', 'watching'));
@@ -15,7 +15,7 @@ clean()
 .then(copy_css)
 .then(sass)
 .then(symlink)
-.then(pack)
+.then(webpack)
 .then(livereload)
 .then(function() {
   if (process.env.NODE_ENV === 'production') {
