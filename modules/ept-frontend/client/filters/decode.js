99f9b46b24b4d235ff36d4cc68ec4502d8bf509e
Anthony Kinsey <anthony@slickage.com>
2019-04-23 14:58:58 -1000

    misc minor fixes

--- modules/ept-frontend/client/filters/decode.js
+++ modules/ept-frontend/client/filters/decode.js
@@ -5,8 +5,9 @@ module.exports = [function () {
   var e = document.createElement('textarea'); // only create one textarea
 
   return function(text) {
+    text = text || '';
     text = text.replace(/(?:&)/gi, '&amp;').replace(/(?:&amp;lt;)/gi, '<').replace(/(?:&amp;gt;)/gi, '>');
-    e.innerHTML = text || '';
+    e.innerHTML = text;
     return e.childNodes[0] ? e.childNodes[0].nodeValue : '';
   };
 }];
