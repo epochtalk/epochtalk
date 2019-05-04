e0c2ed4f2c73466eec1dfbe77a3a889739688fae
Bronson Oka <boka@slickage.com>
2019-05-03 15:50:49 -1000

    add roles/db/delete

--- modules/ept-roles/db/index.js
+++ modules/ept-roles/db/index.js
@@ -3,5 +3,6 @@ var path = require('path');
 module.exports = {
   create: require(path.normalize(__dirname + '/create')),
   all: require(path.normalize(__dirname + '/all')),
-  update: require(path.normalize(__dirname + '/update'))
+  update: require(path.normalize(__dirname + '/update')),
+  delete: require(path.normalize(__dirname + '/delete'))
 };
