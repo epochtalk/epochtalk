2f9caad6acb873010183618365a59867ab32b551
Bronson Oka <boka@slickage.com>
2019-05-06 13:04:23 -1000

    add roles/authorization/index

---
+++ modules/ept-roles/authorization/index.js
@@ -0,0 +1,39 @@
+var path = require('path');
+
+module.exports = [
+  {
+    name: 'auth.roles.all',
+    method: require(path.normalize(__dirname + '/all')),
+    options: { callback: false }
+  },
+  {
+    name: 'auth.roles.update',
+    method: require(path.normalize(__dirname + '/update')),
+    options: { callback: false }
+  },
+  {
+    name: 'auth.roles.delete',
+    method: require(path.normalize(__dirname + '/delete')),
+    options: { callback: false }
+  },
+  {
+    name: 'auth.roles.reprioritize',
+    method: require(path.normalize(__dirname + '/reprioritize')),
+    options: { callback: false }
+  },
+  {
+    name: 'auth.roles.users',
+    method: require(path.normalize(__dirname + '/users')),
+    options: { callback: false }
+  },
+  {
+    name: 'auth.roles.addRoles',
+    method: require(path.normalize(__dirname + '/addRoles')),
+    options: { callback: false }
+  },
+  {
+    name: 'auth.roles.removeRoles',
+    method: require(path.normalize(__dirname + '/removeRoles')),
+    options: { callback: false }
+  }
+];
