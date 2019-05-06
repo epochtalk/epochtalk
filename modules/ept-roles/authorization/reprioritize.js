04d80de02767b7086e3bba6cf8f64c8a27d22922
Bronson Oka <boka@slickage.com>
2019-05-06 13:41:12 -1000

    add roles/authorization/reprioritize

---
+++ modules/ept-roles/authorization/reprioritize.js
@@ -0,0 +1,11 @@
+var boom = require('boom');
+
+module.exports = function(server, auth) {
+  return server.authorization.build({
+    error: boom.forbidden(),
+    type: 'haspermission',
+    server: server,
+    auth: auth,
+    permission: 'adminRoles.reprioritize'
+  });
+};
