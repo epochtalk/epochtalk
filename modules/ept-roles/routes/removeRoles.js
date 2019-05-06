4814239be63d49595709b945e69562b499923f14
Bronson Oka <boka@slickage.com>
2019-05-06 12:59:59 -1000

    update roles/routes/removeRoles
    include route method and path

--- modules/ept-roles/routes/removeRoles.js
+++ modules/ept-roles/routes/removeRoles.js
@@ -16,34 +16,38 @@ var Joi = require('joi');
   * @apiError (Error 500) InternalServerError There was an issue removing the role.
   */
 module.exports = {
-  auth: { strategy: 'jwt' },
-  plugins: {
-    acls: 'adminRoles.remove',
-    mod_log: {
-      type: 'adminRoles.remove',
-      data: {
-        id: 'params.id',
-        name: 'route.settings.plugins.mod_log.metadata.name'
+  method: 'DELETE',
+  path: '/roles/remove/{id}',
+  config: {
+    auth: { strategy: 'jwt' },
+    plugins: {
+      acls: 'adminRoles.remove',
+      mod_log: {
+        type: 'adminRoles.remove',
+        data: {
+          id: 'params.id',
+          name: 'route.settings.plugins.mod_log.metadata.name'
+        }
       }
+    },
+    validate: { params: { id: Joi.string().required() } },
+    pre: [ { method: 'auth.admin.roles.remove(params.id)' } ],
+    handler: function(request, reply) {
+      var id = request.params.id;
+      var promise = request.db.roles.delete(id)
+        .then(function(result) {
+          // Add deleted role name to plugin metadata
+          request.route.settings.plugins.mod_log.metadata = {
+            name: result.name
+          };
+
+          // Remove deleted role from in memory object
+          request.rolesAPI.deleteRole(id);
+          return result;
+        })
+        .error(request.errorMap.toHttpError);
+
+      return reply(promise);
     }
-  },
-  validate: { params: { id: Joi.string().required() } },
-  pre: [ { method: 'auth.admin.roles.remove(params.id)' } ],
-  handler: function(request, reply) {
-    var id = request.params.id;
-    var promise = request.db.roles.delete(id)
-    .then(function(result) {
-      // Add deleted role name to plugin metadata
-      request.route.settings.plugins.mod_log.metadata = {
-        name: result.name
-      };
-
-      // Remove deleted role from in memory object
-      request.rolesAPI.deleteRole(id);
-      return result;
-    })
-    .error(request.errorMap.toHttpError);
-
-    return reply(promise);
   }
 };
