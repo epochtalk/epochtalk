e31e4253b977c146640951059d252eae66ad1984
Bronson Oka <boka@slickage.com>
2019-05-06 12:54:16 -1000

    add roles/routes/index

---
+++ modules/ept-roles/routes/index.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = [
+  require(path.normalize(__dirname + '/all')),
+  require(path.normalize(__dirname + '/update')),
+  require(path.normalize(__dirname + '/delete')),
+  require(path.normalize(__dirname + '/reprioritize')),
+  require(path.normalize(__dirname + '/users')),
+  require(path.normalize(__dirname + '/addRoles')),
+  require(path.normalize(__dirname + '/removeRoles'))
+];
