0286453e6b61565e639f1ecef256eb45868ee1f8
Bronson Oka <boka@slickage.com>
2019-05-06 14:12:20 -1000

    add adminRolesRemove to roles/authorization/removeRoles

--- modules/ept-roles/authorization/removeRoles.js
+++ modules/ept-roles/authorization/removeRoles.js
@@ -1,4 +1,5 @@
-var boom = require('boom');
+var Boom = require('boom');
+var Promise = require('bluebird');
 
 module.exports = function(server, auth) {
   return server.authorization.build({
@@ -6,6 +7,26 @@ module.exports = function(server, auth) {
     type: 'haspermission',
     server: server,
     auth: auth,
-    permission: 'adminRoles.remove'
+    permission: 'adminRoles.remove',
+    method: adminRolesRemove
   });
 };
+
+function adminRolesRemove(roleId){
+  var defaultRoleIds = [
+    'irXvScLORCGVJLtF8onULA', // Super Administrator
+    'BoYOb5rATCqNnEFzQwYvuA', // Administrator
+    '-w9wtzZST32hZgXuaOdCjQ', // Global Moderator
+    'wNOXcRVBS3GRIq8HNsrSPQ', // Moderator
+    '7c2Pd840RDO6hRf5sXo7YA', // User
+    'Z6qgHsx0TD2bP1am9lRwmA', // Banned
+    '2j9S2kjDRIeFm7uyloUD4Q', // Anonymous
+    '80kyFis5QceKXgR1Qo0q9A', // Private
+  ];
+
+  var canDelete = true;
+  if (defaultRoleIds.indexOf(roleId) > -1) {
+    canDelete = Promise.reject(Boom.badRequest('You may not delete the default roles.'));
+  }
+  return canDelete;
+}
