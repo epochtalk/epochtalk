730a7aea47a4eed28a7b3389887b4a82ecbace55
Bronson Oka <boka@slickage.com>
2019-05-23 14:25:02 -1000

    export notifications.latest

--- modules/ept-notifications/db/index.js
+++ modules/ept-notifications/db/index.js
@@ -2,5 +2,6 @@ var path = require('path');
 
 module.exports = {
   create: require(path.normalize(__dirname + '/create')),
-  dismiss: require(path.normalize(__dirname + '/dismiss'))
+  dismiss: require(path.normalize(__dirname + '/dismiss')),
+  latest: require(path.normalize(__dirname + '/latest'))
 };
