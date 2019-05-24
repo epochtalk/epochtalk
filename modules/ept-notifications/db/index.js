6b4a70b8b041042fbbc5f1646907951a39bb3d9f
Bronson Oka <boka@slickage.com>
2019-05-23 14:37:46 -1000

    export notifications.counts

--- modules/ept-notifications/db/index.js
+++ modules/ept-notifications/db/index.js
@@ -3,5 +3,6 @@ var path = require('path');
 module.exports = {
   create: require(path.normalize(__dirname + '/create')),
   dismiss: require(path.normalize(__dirname + '/dismiss')),
-  latest: require(path.normalize(__dirname + '/latest'))
+  latest: require(path.normalize(__dirname + '/latest')),
+  counts: require(path.normalize(__dirname + '/counts'))
 };
