3113a00af976e96824975982abec57f8d9f00847
Bronson Oka <boka@slickage.com>
2019-05-28 20:56:18 -1000

    include routes in ept-notifications/index

--- modules/ept-notifications/index.js
+++ modules/ept-notifications/index.js
@@ -1,7 +1,9 @@
 var path = require('path');
 var db = require(path.normalize(__dirname + '/db'));
+var routes = require(path.normalize(__dirname + '/routes'));
 
 module.exports =  {
   name: 'notifications',
+  routes: routes,
   db: db
 };
