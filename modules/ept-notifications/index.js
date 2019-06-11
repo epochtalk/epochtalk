a982c7be1cb89e7530247d56e83e3848bfd18b9b
Bronson Oka <boka@slickage.com>
2019-06-10 15:47:31 -1000

    export ept-notifications permissions

--- modules/ept-notifications/index.js
+++ modules/ept-notifications/index.js
@@ -3,8 +3,10 @@ var db = require(path.normalize(__dirname + '/db'));
 var routes = require(path.normalize(__dirname + '/routes'));
 var plugins = require(path.normalize(__dirname + '/plugins'));
 var authorization = require(path.normalize(__dirname + '/authorization'));
+var permissions = require(path.normalize(__dirname + '/permissions'));
 
 module.exports =  {
+  permissions: permissions,
   authorization: authorization,
   name: 'notifications',
   routes: routes,
