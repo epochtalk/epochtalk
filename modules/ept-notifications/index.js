c34a18d71d97b6978bfcfbc0e7c37d335dc39237
Bronson Oka <boka@slickage.com>
2019-06-10 15:27:06 -1000

    include authorization in ept-notifications/index

--- modules/ept-notifications/index.js
+++ modules/ept-notifications/index.js
@@ -2,8 +2,10 @@ var path = require('path');
 var db = require(path.normalize(__dirname + '/db'));
 var routes = require(path.normalize(__dirname + '/routes'));
 var plugins = require(path.normalize(__dirname + '/plugins'));
+var authorization = require(path.normalize(__dirname + '/authorization'));
 
 module.exports =  {
+  authorization: authorization,
   name: 'notifications',
   routes: routes,
   db: db,
