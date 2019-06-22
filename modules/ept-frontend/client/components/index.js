27b44cdd5a4af978d702a5a6910f39ef748836f4
Anthony Kinsey <anthony@slickage.com>
2019-06-21 15:48:23 -1000

    Get rid of repetitive code when loading directives

--- modules/ept-frontend/client/components/index.js
+++ modules/ept-frontend/client/components/index.js
@@ -1,13 +1,14 @@
-var app = angular.module('ept');
 // these should be in the main bundle
-app.directive('alert', require('./alert/alert.directive.js'));
-app.directive('pagination', require('./pagination/pagination.directive.js'));
-app.directive('slideToggle', require('./slide_toggle/slide-toggle.directive.js'));
-app.directive('uniqueUsername', require('./unique_username/unique-username.directive.js'));
-app.directive('uniqueInvite', require('./unique_invite/unique-invite.directive.js'));
-app.directive('uniqueEmail', require('./unique_email/unique-email.directive.js'));
-app.directive('modal', require('./modal/modal.directive.js'));
-app.directive('autoFocus', require('./autofocus/autofocus.directive.js'));
-app.directive('modalFocus', require('./modal_focus/modal-focus.directive.js'));
-app.directive('postProcessing', require('./post_processing/post-processing.directive.js'));
-app.directive('jumpToPage', require('./jump_to_page/jump-to-page.directive.js'));
+// the rest are dynamically loaded wherever needed
+angular.module('ept')
+  .directive('alert', require('./alert/alert.directive.js'))
+  .directive('pagination', require('./pagination/pagination.directive.js'))
+  .directive('slideToggle', require('./slide_toggle/slide-toggle.directive.js'))
+  .directive('uniqueUsername', require('./unique_username/unique-username.directive.js'))
+  .directive('uniqueInvite', require('./unique_invite/unique-invite.directive.js'))
+  .directive('uniqueEmail', require('./unique_email/unique-email.directive.js'))
+  .directive('modal', require('./modal/modal.directive.js'))
+  .directive('autoFocus', require('./autofocus/autofocus.directive.js'))
+  .directive('modalFocus', require('./modal_focus/modal-focus.directive.js'))
+  .directive('postProcessing', require('./post_processing/post-processing.directive.js'))
+  .directive('jumpToPage', require('./jump_to_page/jump-to-page.directive.js'));
