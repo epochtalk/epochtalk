213606ceda4facf94c63a1cdeb16d1d328efc578
Bronson Oka <boka@slickage.com>
2019-07-11 15:10:06 -1000

    remove options.callback from modules/ept-legal/authorization/index.js

--- modules/ept-legal/authorization/index.js
+++ modules/ept-legal/authorization/index.js
@@ -3,17 +3,14 @@ var path = require('path');
 module.exports = [
   {
     name: 'auth.legal.reset',
-    method: require(path.normalize(__dirname + '/reset')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/reset'))
   },
   {
     name: 'auth.legal.text',
-    method: require(path.normalize(__dirname + '/text')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/text'))
   },
   {
     name: 'auth.legal.update',
-    method: require(path.normalize(__dirname + '/update')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/update'))
   }
 ];
