a6964ca818c63b53286dd4e835c077ca5ccd54db
Bronson Oka <boka@slickage.com>
2019-07-11 15:10:07 -1000

    remove options.callback from modules/ept-users/authorization/index.js

--- modules/ept-users/authorization/index.js
+++ modules/ept-users/authorization/index.js
@@ -3,67 +3,54 @@ var path = require('path');
 module.exports = [
   {
     name: 'auth.users.update',
-    method: require(path.normalize(__dirname + '/update')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/update'))
   },
   {
     name: 'auth.users.find',
-    method: require(path.normalize(__dirname + '/find')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/find'))
   },
   {
     name: 'auth.users.lookup',
-    method: require(path.normalize(__dirname + '/lookup')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/lookup'))
   },
   {
     name: 'auth.users.metaFind',
-    method: require(path.normalize(__dirname + '/metaFind')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/metaFind'))
   },
   {
     name: 'auth.users.pagePublic',
-    method: require(path.normalize(__dirname + '/pagePublic')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/pagePublic'))
   },
   {
     name: 'auth.users.deactivate',
-    method: require(path.normalize(__dirname + '/deactivate')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/deactivate'))
   },
   {
     name: 'auth.users.activate',
-    method: require(path.normalize(__dirname + '/reactivate')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/reactivate'))
   },
   {
     name: 'auth.users.delete',
-    method: require(path.normalize(__dirname + '/delete')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/delete'))
   },
   {
     name: 'auth.users.adminRecover',
-    method: require(path.normalize(__dirname + '/adminRecover')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/adminRecover'))
   },
   {
     name: 'auth.users.page',
-    method: require(path.normalize(__dirname + '/page')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/page'))
   },
   {
     name: 'auth.users.addRoles',
-    method: require(path.normalize(__dirname + '/addRoles')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/addRoles'))
   },
   {
     name: 'auth.users.removeRole',
-    method: require(path.normalize(__dirname + '/removeRole')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/removeRole'))
   },
   {
     name: 'auth.users.searchUsernames',
-    method: require(path.normalize(__dirname + '/searchUsernames')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/searchUsernames'))
   }
 ];
