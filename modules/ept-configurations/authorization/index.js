863f49db6198e1506594121825d6f6fe17b34083
Bronson Oka <boka@slickage.com>
2019-07-11 15:06:02 -1000

    remove options.callback from modules/ept-configurations/authorization/index.js

--- modules/ept-configurations/authorization/index.js
+++ modules/ept-configurations/authorization/index.js
@@ -3,12 +3,10 @@ var path = require('path');
 module.exports = [
   {
     name: 'auth.configurations.get',
-    method: require(path.normalize(__dirname + '/get')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/get'))
   },
   {
     name: 'auth.configurations.update',
-    method: require(path.normalize(__dirname + '/update')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/update'))
   }
 ];
