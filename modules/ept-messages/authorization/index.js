b1efd95f3e711b821963287928d76fb129acbaa3
Bronson Oka <boka@slickage.com>
2019-07-11 15:10:06 -1000

    remove options.callback from modules/ept-messages/authorization/index.js

--- modules/ept-messages/authorization/index.js
+++ modules/ept-messages/authorization/index.js
@@ -3,33 +3,27 @@ var path = require('path');
 module.exports = [
   {
     name: 'auth.messages.create',
-    method: require(path.normalize(__dirname + '/createMessage')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/createMessage'))
   },
   {
     name: 'auth.messages.latest',
-    method: require(path.normalize(__dirname + '/latest')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/latest'))
   },
   {
     name: 'auth.messages.delete',
-    method: require(path.normalize(__dirname + '/deleteMessage')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/deleteMessage'))
   },
   {
     name: 'auth.conversations.create',
-    method: require(path.normalize(__dirname + '/createConversation')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/createConversation'))
   },
   {
     name: 'auth.conversations.messages',
-    method: require(path.normalize(__dirname + '/messages')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/messages'))
   },
   {
     name: 'auth.conversations.delete',
-    method: require(path.normalize(__dirname + '/deleteConversation')),
-    options: { callback: false }
+    method: require(path.normalize(__dirname + '/deleteConversation'))
   },
 
 ];
