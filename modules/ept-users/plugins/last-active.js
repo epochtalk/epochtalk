0d348838d56f1cbbe36a6e580ae422b5439c880a
Bronson Oka <boka@slickage.com>
2019-07-11 15:46:26 -1000

    reply.continue fix ept-users last-active

--- modules/ept-users/plugins/last-active.js
+++ modules/ept-users/plugins/last-active.js
@@ -7,16 +7,13 @@ module.exports = {
     plugin.ext('onPostAuth', function(request, reply) {
       // check if user is authed
       var authed = request.auth.isAuthenticated;
-      if (!authed) { return reply.continue(); }
+      if (!authed) { return reply.continue; }
 
       // get this user's id
       var userId = request.auth.credentials.id;
 
-      // reply here but continue the rest as a separate process
-      reply.continue();
-
       // get lastActive for this user
-      request.db.users.getLastActive(userId)
+      return request.db.users.getLastActive(userId)
       .then(function(lastActive) {
         // if no lastActive, then just update
         if (!lastActive) { return true; }
@@ -30,7 +27,8 @@ module.exports = {
         return update;
       })
       .then(function(doUpdate) {
-        if (doUpdate) { request.db.users.setLastActive(userId); }
+        if (doUpdate) { return request.db.users.setLastActive(userId); }
+        else { return reply.continue; }
       });
     });
   }
