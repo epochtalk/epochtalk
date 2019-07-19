8a641261788921e06f66fa449a4aca6a5ba37fa9
Anthony Kinsey <anthony@slickage.com>
2019-07-19 12:20:12 -1000

    Get rid of unused server methods

--- server/plugins/common/index.js
+++ server/plugins/common/index.js
@@ -1,35 +1,9 @@
-// -- internal methods
-
-function imagesSite(imageStore, payload) {
-  // clear the expiration on logo/favicon
-  if (payload.website.logo) {
-    imageStore.clearExpiration(payload.website.logo);
-  }
-  if (payload.website.favicon) {
-    imageStore.clearExpiration(payload.website.favicon);
-  }
-}
-
-// -- API
-
 exports.register = function(server, options, next) {
   options = options || {};
   options.methods = options.methods || [];
 
-  // append hardcoded methods to the server
-  var internalMethods = [
-    // -- images
-    {
-      name: 'common.images.site',
-      method: imagesSite,
-      options: { callback: false }
-    }
-  ];
-
-  // append any new methods to methods from options
-  var methods = [].concat(options.methods, internalMethods);
-  server.method(methods);
-
+  // Append all module methods to server
+  server.method(options.methods);
   next();
 };
 
