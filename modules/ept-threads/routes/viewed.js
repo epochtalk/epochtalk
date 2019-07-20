bb8c11786adf04578f60bfb8dbaeafbcfbeb2fa0
Bronson Oka <boka@slickage.com>
2019-07-19 16:06:54 -1000

    return 200 on ept-threads/routes/viewed
    instead of returning nothing

--- modules/ept-threads/routes/viewed.js
+++ modules/ept-threads/routes/viewed.js
@@ -30,6 +30,6 @@ module.exports = {
   handler: function(request, reply) {
     var newViewerId = request.pre.newViewId;
     if (newViewerId) { return reply().header('Epoch-Viewer', newViewerId); }
-    else { return; }
+    else { return 200; }
   }
 };
