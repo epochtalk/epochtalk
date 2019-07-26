2074eaea3eac363381390e8eb27e2bb194bdc0b2
Anthony Kinsey <anthony@slickage.com>
2019-07-26 11:24:53 -1000

    Add return value to db methods returning nothing

--- modules/ept-watchlist/db/watchBoard.js
+++ modules/ept-watchlist/db/watchBoard.js
@@ -6,7 +6,7 @@ var helper = dbc.helper;
 module.exports = function(userId, boardId) {
   userId = helper.deslugify(userId);
   boardId = helper.deslugify(boardId);
-  var q = 'INSERT INTO users.watch_boards (user_id, board_id) VALUES ($1, $2)';
-  return db.sqlQuery(q, [userId, boardId])
-  .then(function() { return; });
+  var q = 'INSERT INTO users.watch_boards (user_id, board_id) VALUES ($1, $2) RETURNING user_id, board_id';
+  return db.scalar(q, [userId, boardId])
+  .then(helper.slugify);
 };
--- modules/ept-watchlist/db/watchThread.js
+++ modules/ept-watchlist/db/watchThread.js
@@ -6,7 +6,7 @@ var helper = dbc.helper;
 module.exports = function(userId, threadId) {
   userId = helper.deslugify(userId);
   threadId = helper.deslugify(threadId);
-  var q = 'INSERT INTO users.watch_threads (user_id, thread_id) VALUES ($1, $2)';
-  return db.sqlQuery(q, [userId, threadId])
-  .then(function() { return; });
+  var q = 'INSERT INTO users.watch_threads (user_id, thread_id) VALUES ($1, $2) RETURNING user_id, thread_id';
+  return db.scalar(q, [userId, threadId])
+  .then(helper.slugify);
 };
