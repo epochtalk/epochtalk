12e77fb32d9ca50fb6c8bbe84404401864158f59
Anthony Kinsey <anthonyjk@gmail.com>
2019-08-01 12:34:08 -1000

    Fix errors coming from track ip when user session expires

--- modules/ept-users/plugins/track-ip.js
+++ modules/ept-users/plugins/track-ip.js
@@ -8,13 +8,14 @@ module.exports = {
     if (!options.db) { return new Error('No DB found in Track IP'); }
     db = options.db;
 
-    server.ext('onPostHandler', function(request, reply) {
+    server.ext('onPostHandler', function(request, h) {
+      var user = request.auth.credentials;
       // If credentials are present and track_ip is true
-      if (request.auth.credentials && _.get(request, 'route.settings.plugins.track_ip')) {
+      if (user && user.id && _.get(request, 'route.settings.plugins.track_ip')) {
         var ip = request.headers['x-forwarded-for'] || request.info.remoteAddress;
-        db.users.trackIp(request.auth.credentials.id, ip);
+        db.users.trackIp(user.id, ip);
       }
-      return reply.continue;
+      return h.continue;
     });
   }
 };
