f653a43d36476dff1427b324d4a079791e02cabf
Anthony Kinsey <anthonyjk@gmail.com>
2019-08-05 14:30:01 -1000

    Send email when mod hides users post

--- modules/ept-posts/routes/delete.js
+++ modules/ept-posts/routes/delete.js
@@ -37,11 +37,39 @@ module.exports = {
       { method: (request) => request.server.methods.auth.posts.lock(request.server, request.auth, request.params.id, request.query) }
     ],
     handler: function(request) {
+      var action = 'hidden';
       var promise = request.db.posts.delete(request)
       .tap(function() {
         if (request.query.locked) {
+          action = 'hidden and locked';
           return request.db.posts.lock(request);
         }
+      })
+      .tap(function(post) {
+        var email;
+        if (post.user_id !== request.auth.credentials.id) {
+          request.db.users.find(post.user_id)
+          .then(function(user) {
+            email = user.email;
+            return request.db.threads.find(post.thread_id);
+          })
+          .then(function(thread) {
+            var config = request.server.app.config;
+            var emailParams = {
+              email: email,
+              mod_username: request.auth.credentials.username,
+              thread_name: thread.title,
+              site_name: config.website.title,
+              thread_url: config.publicUrl + '/threads/' + thread.id + '/posts?start=' + post.position + '#' + post.id,
+              action: action
+            };
+            request.server.log('debug', emailParams);
+            request.emailer.send('postUpdated', emailParams)
+            .catch(console.log);
+            return;
+          });
+        }
+        return;
       })
       .error(request.errorMap.toHttpError);
 
