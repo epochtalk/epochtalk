7f105b1fa537d65286c40c011e71e6322500d647
Anthony Kinsey <anthony@slickage.com>
2019-08-30 15:35:05 -1000

    WIP: connect admin directive to lowdb

--- modules/cb-altcoins/client/admin-projects.directive.js
+++ modules/cb-altcoins/client/admin-projects.directive.js
@@ -1,14 +1,31 @@
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('/modules/cb-altcoins/db/db.json');
+var db = low(adapter);
+
 var directive = ['$timeout', function($timeout) {
   return {
     restrict: 'E',
     scope: true,
     template: require('./admin-projects.html'),
-    controllerAs: 'vm',
+    controllerAs: 'vmAdminProjects',
     controller: [function() {
       var ctrl = this;
+      this.project = null;
 
       $timeout(function() {
+        // Set some defaults
+        db.defaults({ projects: [], tags: [] })
+          .write()
 
+        // Add a post
+        db.get('projects')
+          .push({ id: 1, name: 'Example Coint', ticker: 'EXC'})
+          .write()
+
+        ctrl.project = db.get('projects')
+          .find({ id: 1 })
+          .value()
       });
 
     }]
