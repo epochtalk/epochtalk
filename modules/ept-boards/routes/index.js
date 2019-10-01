0436e6658676895ea388245254f5e8341b238d14
Bronson Oka <boka@slickage.com>
2019-10-01 12:48:28 -1000

    reorder routes alphabetically

--- modules/ept-boards/routes/index.js
+++ modules/ept-boards/routes/index.js
@@ -1,13 +1,13 @@
 var path = require('path');
 
 module.exports = [
-  require(path.normalize(__dirname + '/create')),
-  require(path.normalize(__dirname + '/find')),
-  require(path.normalize(__dirname + '/update')),
-  require(path.normalize(__dirname + '/delete')),
   require(path.normalize(__dirname + '/allCategories')),
-  require(path.normalize(__dirname + '/allUnfiltered')),
   require(path.normalize(__dirname + '/allUncategorized')),
+  require(path.normalize(__dirname + '/allUnfiltered')),
+  require(path.normalize(__dirname + '/create')),
+  require(path.normalize(__dirname + '/delete')),
+  require(path.normalize(__dirname + '/find')),
   require(path.normalize(__dirname + '/moveList')),
+  require(path.normalize(__dirname + '/update')),
   require(path.normalize(__dirname + '/updateAll'))
 ];
