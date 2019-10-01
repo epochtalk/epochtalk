a2f598c8c8d73ddd51896aa8a8c04cd6e8a3c891
Bronson Oka <boka@slickage.com>
2019-09-30 16:40:09 -1000

    wrap in Joi.object

--- modules/ept-ads/routes/ads/duplicate.js
+++ modules/ept-ads/routes/ads/duplicate.js
@@ -40,7 +40,7 @@ module.exports = {
   path: '/api/ads/{id}/duplicate',
   options: {
     auth: { strategy: 'jwt' },
-    validate: { params: { id: Joi.string().required() } },
+    validate: { params: Joi.object({ id: Joi.string().required() }) },
     pre: [ { method: auth } ]
   },
   handler: function(request) {
