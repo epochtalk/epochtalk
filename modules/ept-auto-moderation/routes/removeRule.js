300bfffc0ec36049ab94338b27e43c208b9db1ed
Bronson Oka <boka@slickage.com>
2019-10-01 12:32:15 -1000

    wrap in Joi.object

--- modules/ept-auto-moderation/routes/removeRule.js
+++ modules/ept-auto-moderation/routes/removeRule.js
@@ -35,7 +35,7 @@ module.exports = {
   path: '/api/automoderation/rules/{id}',
   options: {
     auth: { strategy: 'jwt' },
-    validate: { params: { id: Joi.string().required() } },
+    validate: { params: Joi.object({ id: Joi.string().required() }) },
     pre: [ { method: auth } ]
   },
   handler: function(request) {
