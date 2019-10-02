37e76c522389cd1a0cc85994da10e0533ce051ba
Bronson Oka <boka@slickage.com>
2019-10-01 14:50:20 -1000

    move enableMentionEmails to file

--- modules/ept-mentions/routes/index.js
+++ modules/ept-mentions/routes/index.js
@@ -198,40 +198,6 @@ var getMentionEmailSettings = {
   }
 };
 
-
-/**
-  * @apiVersion 0.4.0
-  * @apiGroup Mentions
-  * @api {PUT} /mentions/settings Toggle Mention Emails
-  * @apiName ToggleMentionEmails
-  * @apiPermission User
-  * @apiDescription Used to toggle email notifications when mentioned
-  *
-  * @apiParam (Payload) {boolean} [enabled=true] Boolean indicating if mention emails are enabled or not
-  *
-  * @apiSuccess {boolean} enabled Boolean indicating if the mention emails were enabled or not
-  *
-  * @apiError (Error 500) InternalServerError There was an enabling mention emails
-  */
-var enableMentionEmails = {
-  method: 'PUT',
-  path: '/api/mentions/settings',
-  options: {
-    auth: { strategy: 'jwt' },
-    plugins: { track_ip: true },
-    validate: { payload: { enabled: Joi.boolean().default(true) } }
-  },
-  handler: function(request) {
-    var userId = request.auth.credentials.id;
-    var enabled = request.payload.enabled;
-
-    var promise = request.db.mentions.enableMentionEmails(userId, enabled)
-    .error(request.errorMap.toHttpError);
-
-    return promise;
-  }
-};
-
 module.exports = [
   page: require(path.join(__dirname, 'page')),
   remove,
@@ -239,5 +205,5 @@ module.exports = [
   ignoreUser,
   unignoreUser,
   getMentionEmailSettings,
-  enableMentionEmails
+  enableMentionEmails: require(path.join(__dirname, 'enableMentionEmails'))
 ];
