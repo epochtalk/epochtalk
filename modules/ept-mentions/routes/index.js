b9eb1b31a9cb770ef2838170147e6aa598473309
Bronson Oka <boka@slickage.com>
2019-10-01 15:05:28 -1000

    update file names

--- modules/ept-mentions/routes/index.js
+++ modules/ept-mentions/routes/index.js
@@ -3,9 +3,9 @@ var path = require('path');
 module.exports = [
   page: require(path.join(__dirname, 'page')),
   remove: require(path.join(__dirname, 'remove')),
-  pageIgnoredUsers: require(path.join(__dirname, 'pageIgnoredUsers')),
-  ignoreUser: require(path.join(__dirname, 'ignoreUser')),
-  unignoreUser: require(path.join(__dirname, 'unignoreUser')),
-  getMentionEmailSettings: require(path.join(__dirname, 'getMentionEmailSettings')),
-  enableMentionEmails: require(path.join(__dirname, 'enableMentionEmails'))
+  pageIgnoredUsers: require(path.join(__dirname, 'page-ignored-users')),
+  ignoreUser: require(path.join(__dirname, 'ignore-user')),
+  unignoreUser: require(path.join(__dirname, 'unignore-user')),
+  getMentionEmailSettings: require(path.join(__dirname, 'get-mention-email-settings')),
+  enableMentionEmails: require(path.join(__dirname, 'enable-mention-emails'))
 ];
