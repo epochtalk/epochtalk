ab9c7f1663390478b08b2cd0720b6ddd0ae67d68
Bronson Oka <boka@slickage.com>
2019-10-01 15:11:59 -1000

    reorder route includes alphabetically

--- modules/ept-mentions/routes/index.js
+++ modules/ept-mentions/routes/index.js
@@ -1,11 +1,11 @@
 var path = require('path');
 
 module.exports = [
-  require(path.join(__dirname, 'page')),
-  require(path.join(__dirname, 'remove')),
-  require(path.join(__dirname, 'page-ignored-users')),
-  require(path.join(__dirname, 'ignore-user')),
-  require(path.join(__dirname, 'unignore-user')),
+  require(path.join(__dirname, 'enable-mention-emails')),
   require(path.join(__dirname, 'get-mention-email-settings')),
-  require(path.join(__dirname, 'enable-mention-emails'))
+  require(path.join(__dirname, 'ignore-user')),
+  require(path.join(__dirname, 'page')),
+  require(path.join(__dirname, 'page-ignored-users')),
+  require(path.join(__dirname, 'remove')),
+  require(path.join(__dirname, 'unignore-user'))
 ];
