521300b3ac1456fe1a4d5e9fd9fb3b06c59fba6c
Bronson Oka <boka@slickage.com>
2019-10-01 14:57:44 -1000

    move pageIgnoredUsers to file

--- modules/ept-mentions/routes/index.js
+++ modules/ept-mentions/routes/index.js
@@ -4,55 +4,6 @@ var path = require('path');
 /**
   * @apiVersion 0.4.0
   * @apiGroup Mentions
-  * @api {GET} /mentions/ignored Page Ignored Users
-  * @apiName PageIgnoredUserMentions
-  * @apiPermission User
-  * @apiDescription Used to page through user's whos mentions are being ignored
-  *
-  * @apiParam (Query) {number} [page] The page of ignored users to return
-  * @apiParam (Query) {number} [limit] The number ignored users to return per page
-  *
-  * @apiSuccess {number} page The page of ignored users being returned
-  * @apiSuccess {number} limit The number ignored users being returned per page
-  * @apiSuccess {boolean} prev Boolean indicating if there is a previous page
-  * @apiSuccess {boolean} next Boolean indicating if there is a next page
-  * @apiSuccess {object[]} data Array containing ignored users
-  * @apiSuccess {string} data.username The name of the user being ignored
-  * @apiSuccess {string} data.id The id of the user being ignored
-  * @apiSuccess {string} data.avatar The avatar of the user being ignored
-  * @apiSuccess {boolean} data.ignored Boolean indicating if the user's mentions are being ignored
-  *
-  * @apiError (Error 500) InternalServerError There was an error paging ignored users
-  */
-var pageIgnoredUsers = {
-  method: 'GET',
-  path: '/api/mentions/ignored',
-  options: {
-    auth: { strategy: 'jwt' },
-    plugins: { track_ip: true },
-    validate: {
-      query: {
-        limit: Joi.number(),
-        page: Joi.number()
-      }
-    }
-  },
-  handler: function(request) {
-    var userId = request.auth.credentials.id;
-    var opts = {
-      limit: request.query.limit,
-      page: request.query.page
-    };
-    var promise = request.db.mentions.pageIgnoredUsers(userId, opts)
-    .error(request.errorMap.toHttpError);
-
-    return promise;
-  }
-};
-
-/**
-  * @apiVersion 0.4.0
-  * @apiGroup Mentions
   * @api {POST} /mentions/ignore Ignore User's Mentions
   * @apiName IgnoreUsersMentions
   * @apiPermission User
@@ -159,7 +110,7 @@ var getMentionEmailSettings = {
 module.exports = [
   page: require(path.join(__dirname, 'page')),
   remove: require(path.join(__dirname, 'remove')),
-  pageIgnoredUsers,
+  pageIgnoredUsers: require(path.join(__dirname, 'pageIgnoredUsers')),
   ignoreUser,
   unignoreUser,
   getMentionEmailSettings,
