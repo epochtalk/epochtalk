00430f5e90e149b143353f6ed2ab0f00295811f3
Bronson Oka <boka@slickage.com>
2019-10-01 14:56:09 -1000

    move remove to file

--- modules/ept-mentions/routes/index.js
+++ modules/ept-mentions/routes/index.js
@@ -4,48 +4,6 @@ var path = require('path');
 /**
   * @apiVersion 0.4.0
   * @apiGroup Mentions
-  * @api {DELETE} /mentions Delete Mentions
-  * @apiName DeleteMentions
-  * @apiPermission User
-  * @apiDescription Used to delete a user's mentions
-  *
-  * @apiParam (Query) {string} id The id of the mention to delete
-  *
-  * @apiSuccess {boolean} deleted True if the mention was deleted
-  *
-  * @apiError (Error 500) InternalServerError There was an issue deleting the mention
-  */
-var remove = {
-  method: 'DELETE',
-  path: '/api/mentions',
-  options: {
-    auth: { strategy: 'jwt' },
-    plugins: { track_ip: true },
-    validate: { query: { id: Joi.string() } },
-    pre: [{ method: (request) => request.server.methods.auth.mentions.delete(request.server, request.auth) }],
-  },
-  handler: function(request) {
-    var userId = request.auth.credentials.id;
-    var mentionId = request.query.id;
-    var promise = request.db.mentions.remove(mentionId, userId)
-    .tap(function() {
-      var notification = {
-        channel: { type: 'user', id: userId },
-        data: { action: 'refreshMentions' }
-      };
-      request.server.plugins.notifications.systemNotification(notification);
-    })
-    .error(request.errorMap.toHttpError);
-
-    return promise;
-  }
-};
-
-
-
-/**
-  * @apiVersion 0.4.0
-  * @apiGroup Mentions
   * @api {GET} /mentions/ignored Page Ignored Users
   * @apiName PageIgnoredUserMentions
   * @apiPermission User
@@ -200,7 +158,7 @@ var getMentionEmailSettings = {
 
 module.exports = [
   page: require(path.join(__dirname, 'page')),
-  remove,
+  remove: require(path.join(__dirname, 'remove')),
   pageIgnoredUsers,
   ignoreUser,
   unignoreUser,
---
+++ modules/ept-mentions/routes/remove.js
@@ -0,0 +1,41 @@
+var Joi = require('@hapi/joi');
+
+/**
+  * @apiVersion 0.4.0
+  * @apiGroup Mentions
+  * @api {DELETE} /mentions Delete Mentions
+  * @apiName DeleteMentions
+  * @apiPermission User
+  * @apiDescription Used to delete a user's mentions
+  *
+  * @apiParam (Query) {string} id The id of the mention to delete
+  *
+  * @apiSuccess {boolean} deleted True if the mention was deleted
+  *
+  * @apiError (Error 500) InternalServerError There was an issue deleting the mention
+  */
+var remove = {
+  method: 'DELETE',
+  path: '/api/mentions',
+  options: {
+    auth: { strategy: 'jwt' },
+    plugins: { track_ip: true },
+    validate: { query: { id: Joi.string() } },
+    pre: [{ method: (request) => request.server.methods.auth.mentions.delete(request.server, request.auth) }],
+  },
+  handler: function(request) {
+    var userId = request.auth.credentials.id;
+    var mentionId = request.query.id;
+    var promise = request.db.mentions.remove(mentionId, userId)
+    .tap(function() {
+      var notification = {
+        channel: { type: 'user', id: userId },
+        data: { action: 'refreshMentions' }
+      };
+      request.server.plugins.notifications.systemNotification(notification);
+    })
+    .error(request.errorMap.toHttpError);
+
+    return promise;
+  }
+};
