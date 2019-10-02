27ca6f345a2716fbb7f302243662552364500786
Bronson Oka <boka@slickage.com>
2019-10-01 15:10:14 -1000

    fix route include array

--- modules/ept-mentions/routes/index.js
+++ modules/ept-mentions/routes/index.js
@@ -1,11 +1,11 @@
 var path = require('path');
 
 module.exports = [
-  page: require(path.join(__dirname, 'page')),
-  remove: require(path.join(__dirname, 'remove')),
-  pageIgnoredUsers: require(path.join(__dirname, 'page-ignored-users')),
-  ignoreUser: require(path.join(__dirname, 'ignore-user')),
-  unignoreUser: require(path.join(__dirname, 'unignore-user')),
-  getMentionEmailSettings: require(path.join(__dirname, 'get-mention-email-settings')),
-  enableMentionEmails: require(path.join(__dirname, 'enable-mention-emails'))
+  require(path.join(__dirname, 'page')),
+  require(path.join(__dirname, 'remove')),
+  require(path.join(__dirname, 'page-ignored-users')),
+  require(path.join(__dirname, 'ignore-user')),
+  require(path.join(__dirname, 'unignore-user')),
+  require(path.join(__dirname, 'get-mention-email-settings')),
+  require(path.join(__dirname, 'enable-mention-emails'))
 ];
