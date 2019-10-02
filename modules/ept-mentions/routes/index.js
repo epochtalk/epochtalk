c79c047daf675f9ed1c32c13662353874d72d5bb
Bronson Oka <boka@slickage.com>
2019-10-01 15:00:26 -1000

    move unignoreUser to file

--- modules/ept-mentions/routes/index.js
+++ modules/ept-mentions/routes/index.js
@@ -4,48 +4,6 @@ var path = require('path');
 /**
   * @apiVersion 0.4.0
   * @apiGroup Mentions
-  * @api {POST} /mentions/unignore Unignore User's Mentions
-  * @apiName UnignoreUsersMentions
-  * @apiPermission User
-  * @apiDescription Used to unignore mentions from a specific user's
-  *
-  * @apiParam (Payload) {string} username The name of the user to unignore mentions from
-  *
-  * @apiSuccess {boolean} success True if the user was unignored
-  *
-  * @apiError (Error 500) InternalServerError There was an issue unignoring mentions
-  */
-var unignoreUser = {
-  method: 'POST',
-  path: '/api/mentions/unignore',
-  options: {
-    auth: { strategy: 'jwt' },
-    plugins: { track_ip: true },
-    validate: { payload: { username: Joi.string() } }
-  },
-  handler: function(request) {
-    var userId = request.auth.credentials.id;
-    var ignoredUser = request.payload.username;
-    var promise;
-    if (ignoredUser) { // unignore one
-      promise = request.db.users.userByUsername(ignoredUser)
-      .then(function(user) {
-        return request.db.mentions.unignoreUser(userId, user.id);
-      })
-      .error(request.errorMap.toHttpError);
-    }
-    else { // unignore all
-      promise = request.db.mentions.unignoreUser(userId)
-      .error(request.errorMap.toHttpError);
-    }
-    return promise;
-  }
-};
-
-
-/**
-  * @apiVersion 0.4.0
-  * @apiGroup Mentions
   * @api {GET} /mentions/settings Get Mention Settings
   * @apiName GetMentionSettings
   * @apiPermission User
@@ -77,7 +35,7 @@ module.exports = [
   remove: require(path.join(__dirname, 'remove')),
   pageIgnoredUsers: require(path.join(__dirname, 'pageIgnoredUsers')),
   ignoreUser: require(path.join(__dirname, 'ignoreUser')),
-  unignoreUser,
+  unignoreUser: require(path.join(__dirname, 'unignoreUser')),
   getMentionEmailSettings,
   enableMentionEmails: require(path.join(__dirname, 'enableMentionEmails'))
 ];
---
+++ modules/ept-mentions/routes/unignoreUser.js
@@ -0,0 +1,42 @@
+var Joi = require('@hapi/joi');
+
+/**
+  * @apiVersion 0.4.0
+  * @apiGroup Mentions
+  * @api {POST} /mentions/unignore Unignore User's Mentions
+  * @apiName UnignoreUsersMentions
+  * @apiPermission User
+  * @apiDescription Used to unignore mentions from a specific user's
+  *
+  * @apiParam (Payload) {string} username The name of the user to unignore mentions from
+  *
+  * @apiSuccess {boolean} success True if the user was unignored
+  *
+  * @apiError (Error 500) InternalServerError There was an issue unignoring mentions
+  */
+var unignoreUser = {
+  method: 'POST',
+  path: '/api/mentions/unignore',
+  options: {
+    auth: { strategy: 'jwt' },
+    plugins: { track_ip: true },
+    validate: { payload: { username: Joi.string() } }
+  },
+  handler: function(request) {
+    var userId = request.auth.credentials.id;
+    var ignoredUser = request.payload.username;
+    var promise;
+    if (ignoredUser) { // unignore one
+      promise = request.db.users.userByUsername(ignoredUser)
+      .then(function(user) {
+        return request.db.mentions.unignoreUser(userId, user.id);
+      })
+      .error(request.errorMap.toHttpError);
+    }
+    else { // unignore all
+      promise = request.db.mentions.unignoreUser(userId)
+      .error(request.errorMap.toHttpError);
+    }
+    return promise;
+  }
+};
