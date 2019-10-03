0b7746600805525a7319a9e23c5eca605dd8b686
Bronson Oka <boka@slickage.com>
2019-10-03 12:35:24 -1000

    wrap in Joi.object

--- modules/ept-posts/routes/find.js
+++ modules/ept-posts/routes/find.js
@@ -30,7 +30,7 @@ module.exports = {
   options: {
     app: { hook: 'posts.find' },
     auth: { mode: 'try', strategy: 'jwt' },
-    validate: { params: { id: Joi.string().required() } },
+    validate: { params: Joi.object({ id: Joi.string().required() }) },
     pre: [
       { method: (request) => request.server.methods.auth.posts.find(request.server, request.auth, request.params.id), assign: 'viewDeleted' },
       { method: (request) => request.server.methods.hooks.preProcessing(request) },
