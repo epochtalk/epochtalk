bdd7a4a7e8cbd459e517e8feed3da1a1278a99c4
Bronson Oka <boka@slickage.com>
2019-10-04 13:23:47 -1000

    reorder routes alphabetically

--- modules/ept-user-notes/routes/index.js
+++ modules/ept-user-notes/routes/index.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = [
   require(path.normalize(__dirname + '/create')),
-  require(path.normalize(__dirname + '/update')),
   require(path.normalize(__dirname + '/delete')),
-  require(path.normalize(__dirname + '/page'))
+  require(path.normalize(__dirname + '/page')),
+  require(path.normalize(__dirname + '/update'))
 ];
