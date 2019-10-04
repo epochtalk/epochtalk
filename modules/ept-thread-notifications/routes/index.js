66663069f3171177630392a50dbca6f6f9d5b9c4
Bronson Oka <boka@slickage.com>
2019-10-04 13:05:29 -1000

    move get-notification-settings route to file

--- modules/ept-thread-notifications/routes/index.js
+++ modules/ept-thread-notifications/routes/index.js
@@ -1,33 +1,5 @@
 var Joi = require('@hapi/joi');
 
-/**
-  * @apiVersion 0.4.0
-  * @apiGroup ThreadNotifications
-  * @api {GET} /threadnotifications Get Thread Notification Settings
-  * @apiName GetThreadNotificationSettings
-  * @apiPermission User
-  * @apiDescription Used to retreive the user's thread notification settings
-  *
-  * @apiSuccess {boolean} notify_replied_threads Boolean indicating if the user is receiving thread notifications
-  *
-  * @apiError (Error 500) InternalServerError There was an getting thread notification settings
-  */
-var getNotificationSettings = {
-  method: 'GET',
-  path: '/api/threadnotifications',
-  options: {
-    auth: { strategy: 'jwt' },
-    plugins: { track_ip: true }
-  },
-  handler: function(request) {
-    var userId = request.auth.credentials.id;
-
-    var promise = request.db.threadNotifications.getNotificationSettings(userId)
-    .error(request.errorMap.toHttpError);
-
-    return promise;
-  }
-};
 
 
 /**
