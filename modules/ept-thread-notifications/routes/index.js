bb5b4d79d6e308e751ec0a7b7ba5da49b48a5564
Bronson Oka <boka@slickage.com>
2019-10-04 13:06:26 -1000

    move remove-subscriptions route to file

--- modules/ept-thread-notifications/routes/index.js
+++ modules/ept-thread-notifications/routes/index.js
@@ -35,35 +35,6 @@ var enableNotifications = {
   }
 };
 
-/**
-  * @apiVersion 0.4.0
-  * @apiGroup Mentions
-  * @api {DELETE} /threadnotifications Remove Thread Subscriptions
-  * @apiName RemoveThreadSubscriptions
-  * @apiPermission User
-  * @apiDescription Used to delete a user's thread subscriptions
-  *
-  * @apiSuccess {boolean} deleted True if the user's thread subscriptions were deleted
-  *
-  * @apiError (Error 500) InternalServerError There was an issue deleting the user's thread subscriptions
-  */
-var removeSubscriptions = {
-  method: 'DELETE',
-  path: '/api/threadnotifications',
-  options: {
-    auth: { strategy: 'jwt' },
-    plugins: { track_ip: true }
-  },
-  handler: function(request) {
-    var userId = request.auth.credentials.id;
-
-    var promise = request.db.threadNotifications.removeSubscriptions(userId)
-    .error(request.errorMap.toHttpError);
-
-    return promise;
-  }
-};
-
 module.exports = [
   getNotificationSettings,
   enableNotifications,
