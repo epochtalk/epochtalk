0cc6db36f814f09ff9cb24d8e3e2993a0de56c0e
Bronson Oka <boka@slickage.com>
2019-10-04 13:45:26 -1000

    wrap in Joi.object

--- modules/ept-watchlist/routes/unwatchBoard.js
+++ modules/ept-watchlist/routes/unwatchBoard.js
@@ -19,7 +19,7 @@ module.exports = {
   path: '/api/watchlist/boards/{id}',
   options: {
     auth: { strategy: 'jwt' },
-    validate: { params: { id: Joi.string().required() } },
+    validate: { params: Joi.object({ id: Joi.string().required() }) },
     pre: [ { method: (request) => request.server.methods.auth.watchlist.unwatchBoard(request.server, request.auth) } ]
   },
   handler: function(request) {
