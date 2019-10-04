165970c0744b52c61f6c89658881768af106f4f3
Bronson Oka <boka@slickage.com>
2019-10-04 13:17:51 -1000

    wrap in Joi.object

--- modules/ept-threads/routes/meta-byBoard.js
+++ modules/ept-threads/routes/meta-byBoard.js
@@ -6,7 +6,7 @@ module.exports = {
   options: {
     app: { hook: 'threads.byBoard' },
     auth: { mode: 'try', strategy: 'jwt' },
-    validate: { params: { board_id: Joi.string().required() } },
+    validate: { params: Joi.object({ board_id: Joi.string().required() }) },
     pre: [
       { method: (request) => request.server.methods.auth.threads.metaByBoard(request.server, request.auth, request.params.board_id), assign: 'viewable' }
     ],
