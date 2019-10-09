c6d7b62695167cc8320ab0820c313e06a1255b9a
Anthony Kinsey <anthonyjk@gmail.com>
2019-10-08 14:47:11 -1000

    Modify User thread query to not need page count
    * With a large dataset, counting posts(thread starting posts) by a
    particular user will be very costly. To save time and computational
    power, we can just allow the user to hit Next or Prev instead of
    showing a hard number of pages...

--- modules/ept-posts/db/pageFirstPostByUser.js
+++ modules/ept-posts/db/pageFirstPostByUser.js
@@ -5,6 +5,19 @@ var db = dbc.db;
 var helper = dbc.helper;
 
 module.exports = function(username, priority, opts) {
+  // Defaults
+  var limit = 25;
+  var page = 1;
+
+  if (opts.limit) { limit = opts.limit; }
+  else { opts.limit = limit; }
+  if (opts.page) { page = opts.page; }
+  else { opts.page = page; }
+
+  // Build results object for return
+  var results = Object.assign({}, opts);
+  results.prev = results.page > 1 ? results.page - 1 : undefined;
+
   var userq = `SELECT id FROM users WHERE username = $1`;
   return db.sqlQuery(userq, [username])
   .then(function(rows) {
@@ -43,16 +56,24 @@ module.exports = function(username, priority, opts) {
     LEFT JOIN boards b ON t.board_id = b.id
     WHERE p.user_id = $1 AND p.position = 1 ORDER BY created_at`;
 
-    opts = opts || {};
-    var limit = opts.limit || 25;
-    var page = opts.page || 1;
     var order = opts.desc ? 'DESC' : 'ASC';
+    // Calculate pagination vars
     var offset = (page * limit) - limit;
+    limit = limit + 1; // query one extra result to see if theres another page
+
 
     q = [q, order, 'LIMIT $3 OFFSET $4'].join(' ');
     var params = [userId, priority, limit, offset];
     return db.sqlQuery(q, params)
     .map(common.formatPost)
-    .then(helper.slugify);
+    .then(function(data) {
+      // Check for next page then remove extra record
+      if (data.length === limit) {
+        results.next = page + 1;
+        data.pop();
+      }
+      results.data = helper.slugify(data);
+      return results;
+    });
   });
 };
