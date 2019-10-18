1438cd1e4048ecb72adad4d7f3007e2a01ad84d7
Anthony Kinsey <anthonyjk@gmail.com>
2019-10-18 12:35:14 -1000

    Fix for bug in client code, caused by mod changes

--- modules/ept-posts/client/posts/posts.controller.js
+++ modules/ept-posts/client/posts/posts.controller.js
@@ -16,9 +16,11 @@ var ctrl = [
     this.user = Session.user;
     this.posts = pageData.posts;
     this.thread = pageData.thread;
+    pageData.board.moderators = pageData.board.moderators || [];
     this.moderators = pageData.board.moderators.map(function(data) {
       return data.id;
     });
+
     this.loadEditor = parent.loadEditor;
     this.addQuote = parent.addQuote;
     this.openReportModal = parent.openReportModal;
