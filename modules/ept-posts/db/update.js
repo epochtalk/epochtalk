eb9bf3f3e1bbe5b1b3df260b5542534fbdee1601
Anthony Kinsey <anthonyjk@gmail.com>
2019-11-05 14:45:06 -1000

    fix: delete edit meta when editing own post

--- modules/ept-posts/db/update.js
+++ modules/ept-posts/db/update.js
@@ -9,9 +9,10 @@ var NotFoundError = errors.NotFoundError;
 
 module.exports = function(post, authedUser) {
   post = helper.deslugify(post);
+  var authedId = helper.deslugify(authedUser.id);
   return using(db.createTransaction(), function(client) {
     var q, params;
-    q = 'SELECT content ->> \'title\' as title, content ->> \'body\' as body, metadata, created_at FROM posts WHERE id = $1 FOR UPDATE';
+    q = 'SELECT content ->> \'title\' as title, content ->> \'body\' as body, metadata, created_at, user_id FROM posts WHERE id = $1 FOR UPDATE';
     return client.query(q, [post.id])
     .then(function(results) {
       if (results.rows.length > 0) { return results.rows[0]; }
@@ -21,20 +22,24 @@ module.exports = function(post, authedUser) {
       post.title = post.title || oldPost.title;
       helper.updateAssign(post, oldPost, post, 'body');
       post.thread_id = post.thread_id || oldPost.thread_id;
-      if (authedUser.id !== post.user_id) {
-        post.metadata = oldPost.metadata || {};
-        post.metadata.edited_by_id = authedUser.id;
+      post.metadata = oldPost.metadata || {};
+      if (authedId !== oldPost.user_id) {
+        post.metadata.edited_by_id = authedId;
         post.metadata.edited_by_username = authedUser.username;
+      }
+      else {
+        delete post.metadata.edited_by_id;
+        delete post.metadata.edited_by_username;
       }
     })
     .then(function() {
       let post_content = {title: post.title, body: post.body};
       if (post.metadata) {
-        q = 'UPDATE posts SET content = $1, thread_id = $2, metadata = $3, updated_at = now() WHERE id = $4 RETURNING updated_at, position, user_id';
+        q = 'UPDATE posts SET content = $1, thread_id = $2, metadata = $3, updated_at = now() WHERE id = $4 RETURNING updated_at, position, user_id, metadata';
         params = [post_content, post.thread_id, post.metadata, post.id];
       }
       else {
-        q = 'UPDATE posts SET content = $1, thread_id = $2, updated_at = now() WHERE id = $3 RETURNING updated_at, position, user_id';
+        q = 'UPDATE posts SET content = $1, thread_id = $2, updated_at = now() WHERE id = $3 RETURNING updated_at, position, user_id, metadata';
         params = [post_content, post.thread_id, post.id];
       }
       return client.query(q, params)
@@ -43,6 +48,7 @@ module.exports = function(post, authedUser) {
         post.updated_at = dbPost.updated_at;
         post.position = dbPost.position;
         post.user_id = dbPost.user_id;
+        post.metadata = dbPost.metadata;
         return post;
       });
     });
