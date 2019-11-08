248c6670e4d8e57940cb32d1c69182ecf9949bd1
Anthony Kinsey <anthonyjk@gmail.com>
2019-11-08 13:15:28 -1000

    fix: add missing properties for disable_post_edit and fix logic

--- modules/ept-boards/client/nestable-boards.directive.js
+++ modules/ept-boards/client/nestable-boards.directive.js
@@ -104,6 +104,7 @@ var directive = ['$compile', function($compile) {
         $('#newBoardViewable').val('');
         $('#newBoardPostable').val('');
         $('#ltr').prop('checked', true);
+        $('#new-enable-post-edit').prop('checked', true);
       };
 
     }
