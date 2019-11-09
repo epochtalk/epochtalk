2c056244d89cb95b9a0ca2a7913c005d7721791e
Anthony Kinsey <anthonyjk@gmail.com>
2019-11-08 15:47:32 -1000

    feat: add authorization checking if post edit is locked for board on post update

--- modules/ept-posts/authorization/update.js
+++ modules/ept-posts/authorization/update.js
@@ -120,6 +120,42 @@ module.exports = function postsUpdate(server, auth, postId, threadId) {
   ];
   var tLocked = server.authorization.stitch(Boom.forbidden('locked'), tLockedCond, 'any');
 
+  // is thread locked
+  var bLockedCond = [
+    {
+      // permission based override
+      type: 'hasPermission',
+      server: server,
+      auth: auth,
+      permission: 'posts.update.bypass.locked.admin'
+    },
+    {
+      // is board post editing locked
+      type: 'dbNotProp',
+      method: function(threadId) {
+        return server.db.threads.find(threadId)
+        .then(function(thread) {
+          return server.db.boards.find(thread.board_id)
+        });
+      },
+      args: [threadId],
+      prop: 'disable_post_edit'
+    },
+    {
+      // is board moderator
+      type: 'isMod',
+      method: server.db.moderators.isModeratorWithThreadId,
+      args: [userId, threadId],
+      permission: server.plugins.acls.getACLValue(auth, 'posts.update.bypass.locked.mod')
+    },
+    {
+      type: 'runValidation',
+      method: common.hasPriority,
+      args: [server, auth, 'posts.update.bypass.locked.priority', postId]
+    }
+  ];
+  var bLocked = server.authorization.stitch(Boom.forbidden('Post editing is disabled for this board'), bLockedCond, 'any');
+
   var pLocked = postLocked(server, auth, postId);
 
   // read board
@@ -147,7 +183,7 @@ module.exports = function postsUpdate(server, auth, postId, threadId) {
   });
 
   // final promise
-  return Promise.all([allowed, owner, deleted, read, write, tLocked, pLocked, active]);
+  return Promise.all([allowed, owner, deleted, read, write, tLocked, bLocked, pLocked, active]);
 };
 
 function postLocked(server, auth, postId) {
