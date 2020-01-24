c67a64e6e941a1d9dfdc1c32571df8cc665dc5a3
Bronson Oka <boka@slickage.com>
2020-01-23 17:49:51 -1000

    feat: set useful default for tz offset hours

--- modules/ept-users/client/settings/settings.controller.js
+++ modules/ept-users/client/settings/settings.controller.js
@@ -47,21 +47,22 @@ var ctrl = ['$rootScope', '$location', 'PreferencesSvc', 'User', 'Alert', 'Sessi
       { value: '45', label: '45' }
     ];
     this.timezone_offset_hours_options = [
-      '00',
-      '01',
-      '02',
-      '03',
-      '04',
-      '05',
-      '06',
-      '07',
-      '08',
-      '09',
-      '10',
-      '11',
-      '12',
-      '13',
-      '14'
+      { value: '', label: 'HH', disabled: true },
+      { value: '00', label: '00' },
+      { value: '01', label: '01' },
+      { value: '02', label: '02' },
+      { value: '03', label: '03' },
+      { value: '04', label: '04' },
+      { value: '05', label: '05' },
+      { value: '06', label: '06' },
+      { value: '07', label: '07' },
+      { value: '08', label: '08' },
+      { value: '09', label: '09' },
+      { value: '10', label: '10' },
+      { value: '11', label: '11' },
+      { value: '12', label: '12' },
+      { value: '13', label: '13' },
+      { value: '14', label: '14' }
     ];
     function timezoneOffsetValid() {
       return ctrl.timezone_offset_sign !== '' && ctrl.timezone_offset_hours !== '' && ctrl.timezone_offset_minutes !== '';
