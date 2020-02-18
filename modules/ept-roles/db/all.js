30c2eb988b2a0ef3e1d8fa91aba5b5fc7b2e1c42
Anthony Kinsey <anthonyjk@gmail.com>
2020-02-18 12:58:50 -1000

    fix(permissions-upgrade): fix issue with roles management view not working with null permissions

--- modules/ept-roles/db/all.js
+++ modules/ept-roles/db/all.js
@@ -6,5 +6,9 @@ var helper = dbc.helper;
 module.exports = function() {
   var q = 'SELECT id, name, description, lookup, priority, highlight_color, base_permissions, custom_permissions AS permissions FROM roles ORDER BY priority';
   return db.sqlQuery(q)
+  .map(function(row) {
+    row.permissions = row.permissions || {};
+    return row;
+  })
   .then(helper.slugify);
 };
