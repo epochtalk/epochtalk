48dfdd2fff3b181c9ae979bbb21bc9022254474f
Anthony Kinsey <anthonyjk@gmail.com>
2020-03-03 12:18:49 -1000

    feat: bring back thread started by username with newbie posts

--- modules/bct-patroller/db/publicNewbiePosts.js
+++ modules/bct-patroller/db/publicNewbiePosts.js
@@ -4,7 +4,17 @@ var db = dbc.db;
 var helper = dbc.helper;
 
 module.exports = function(request) {
-  var q = 'SELECT p.*, t.*, u.username FROM posts p LEFT JOIN threads t ON p.thread_id = t.id LEFT JOIN users u ON p.user_id = u.id WHERE p.user_id IN (SELECT user_id FROM roles_users WHERE role_id = (SELECT id FROM roles WHERE lookup = \'newbie\')) ORDER BY p.created_at LIMIT 300;';
+  var q = `SELECT
+      p.*,
+      t.*,
+      (SELECT u2.username FROM posts p2 LEFT JOIN users u2 ON u2.id = p2.user_id WHERE p2.thread_id = p.thread_id ORDER BY p2.created_at ASC LIMIT 1) AS thread_started_by_username,
+      u.username FROM posts p
+    LEFT JOIN threads t ON p.thread_id = t.id
+    LEFT JOIN users u ON p.user_id = u.id
+    WHERE p.user_id IN
+      (SELECT user_id FROM roles_users WHERE role_id = (SELECT id FROM roles WHERE lookup = 'newbie')) AND p.deleted = FALSE
+    ORDER BY p.created_at
+    LIMIT 200;`;
   return db.sqlQuery(q)
   .map(function(post) {
     // Build the breadcrumbs and reply
