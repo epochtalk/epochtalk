b1dfbcaa728f0f0bd815bf58f9e9b9884098716b
Anthony Kinsey <anthony@slickage.com>
2020-03-05 14:40:45 -1000

    feat: set online status of users on newbie post page

--- modules/bct-patroller/client/newbie.controller.js
+++ modules/bct-patroller/client/newbie.controller.js
@@ -1,6 +1,28 @@
 var ctrl = ['Alert', 'pageData', function(Alert, pageData) {
     var ctrl = this;
     this.posts = pageData;
+
+    function checkUsersOnline() {
+      var uniqueUsers = {};
+      ctrl.posts.forEach(function(post) {
+        uniqueUsers[post.user.id] = 'user';
+      });
+
+      Object.keys(uniqueUsers).map(function(user) {
+        return Websocket.isOnline(user, setOnline);
+      });
+    }
+
+    function setOnline(err, data) {
+      if (err) { return console.log(err); }
+      else {
+        ctrl.posts.map(function(post) {
+          if (post.user.id === data.id) {
+            post.user.online = data.online;
+          }
+        });
+      }
+    }
   }
 ];
 
