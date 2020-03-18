fea55c656de1782707741aba5680799b3e781f48
Anthony Kinsey <anthonyjk@gmail.com>
2020-03-17 14:49:45 -1000

    fix: fix issue with hidden by metadata on post causing permissions issues. fixes #787

--- modules/ept-posts/common/index.js
+++ modules/ept-posts/common/index.js
@@ -206,8 +206,8 @@ function cleanPosts(posts, currentUserId, viewContext, request, thread, allowVie
     viewablesType = 'array';
   }
   return posts.map(function(post) {
-    var postHiddenByPriority = post.metadata ? post.metadata.hidden_by_priority : post.user.priority;
-    var postHiddenById = post.metadata ? post.metadata.hidden_by_id : post.user.id;
+    var postHiddenByPriority = post.metadata && Object.keys(post.metadata).length ? post.metadata.hidden_by_priority : post.user.priority;
+    var postHiddenById = post.metadata && Object.keys(post.metadata).length ? post.metadata.hidden_by_id : post.user.id;
     var authedUserHasPriority = authedUserPriority <= postHiddenByPriority;
     var authedUserHidePost = postHiddenById === authedId;
 
