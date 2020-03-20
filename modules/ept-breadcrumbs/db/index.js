6a8c0e8562c3a57415aa400eaf0824407d45126c
Bronson Oka <boka@slickage.com>
2020-03-19 16:53:24 -1000

    style: remove extra comma

--- modules/ept-breadcrumbs/db/index.js
+++ modules/ept-breadcrumbs/db/index.js
@@ -1,5 +1,5 @@
 var path = require('path');
 
 module.exports = {
-  getBreadcrumbs: require(path.normalize(__dirname + '/getBreadcrumbs')),
+  getBreadcrumbs: require(path.normalize(__dirname + '/getBreadcrumbs'))
 };
