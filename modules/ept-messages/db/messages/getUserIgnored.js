77dd3d02686dc442b052d8590945e1c35e01cf27
Anthony Kinsey <anthonyjk@gmail.com>
2020-04-27 10:29:36 -1000

    refactor: fix db method for determining if a user is ignored

--- modules/ept-messages/db/messages/getUserIgnored.js
+++ modules/ept-messages/db/messages/getUserIgnored.js
@@ -6,10 +6,11 @@ var db = dbc.db;
 module.exports = function(userId, ignoredUserId) {
   userId = helper.deslugify(userId);
   ignoredUserId = helper.deslugify(ignoredUserId);
-  var q = 'SELECT ignored_user_id FROM mentions.ignored WHERE user_id = $1 AND ignored_user_id = $2';
+  var q = 'SELECT ignored_user_id, u.username FROM messages.ignored LEFT JOIN users u ON u.id = ignored_user_id WHERE user_id = $1 AND ignored_user_id = $2';
   return db.scalar(q, [userId, ignoredUserId])
-  .then(function(ignoredUserId) {
-    return { ignored: ignoredUserId ? true : false };
+  .then(function(data) {
+    if (data) { return { ignored: true, ignored_username: data.username }; }
+    else { return { ignored: false }; }
   });
 };
 
