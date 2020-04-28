6ccb127c823c1ac16f90676a098f133e7e83fb3e
Anthony Kinsey <anthonyjk@gmail.com>
2020-04-28 12:47:57 -1000

    feat: bring back more data on ignored users, ignore users both ways

--- modules/ept-messages/db/messages/getUserIgnored.js
+++ modules/ept-messages/db/messages/getUserIgnored.js
@@ -6,10 +6,10 @@ var db = dbc.db;
 module.exports = function(userId, ignoredUserId) {
   userId = helper.deslugify(userId);
   ignoredUserId = helper.deslugify(ignoredUserId);
-  var q = 'SELECT ignored_user_id, u.username FROM messages.ignored LEFT JOIN users u ON u.id = ignored_user_id WHERE user_id = $1 AND ignored_user_id = $2';
+  var q = 'SELECT ignored_user_id, u.username FROM messages.ignored LEFT JOIN users u ON u.id = user_id WHERE (user_id = $1 AND ignored_user_id = $2) OR (user_id = $2 AND ignored_user_id = $1)';
   return db.scalar(q, [userId, ignoredUserId])
   .then(function(data) {
-    if (data) { return { ignored: true, ignored_username: data.username }; }
+    if (data) { return { ignored: true, ignored_username: data.username, ignored_user_id: helper.slugify(data.ignored_user_id) }; }
     else { return { ignored: false }; }
   });
 };
