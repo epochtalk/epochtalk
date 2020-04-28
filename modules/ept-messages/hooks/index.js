ed3cf74cae4fd285d150671f6c8e8c7233185ddf
Anthony Kinsey <anthonyjk@gmail.com>
2020-04-27 14:13:35 -1000

    feat: add hook to check if user is ignored before sending messages

---
+++ modules/ept-messages/hooks/index.js
@@ -0,0 +1,21 @@
+var Promise = require('bluebird');
+var Boom = require('boom');
+
+function checkUserIgnoredMessages(request) {
+  var receivers = request.payload.receiver_ids;
+  var authedUserId;
+  if (request.auth.isAuthenticated) { authedUserId = request.auth.credentials.id; }
+  else { return Boom.unauthorized('You must log in to perform this action'); }
+  return Promise.each(receivers, function(receiverId) {
+    return request.db.messages.getUserIgnored(authedUserId, receiverId)
+    .then(function(data) {
+      if (data.ignored) { return Promise.reject(Boom.forbidden('One or more of the users you are trying to send a message to has choosen to ignore you.')); }
+      else { return true; }
+    });
+  });
+}
+
+module.exports = [
+  { path: 'conversations.create.pre', method: checkUserIgnoredMessages },
+  { path: 'messages.create.pre', method: checkUserIgnoredMessages }
+];
