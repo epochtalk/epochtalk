39461c0b7681f4efc190741b4cf8eeb6f3fd09cc
Anthony Kinsey <anthonyjk@gmail.com>
2020-04-28 11:55:48 -1000

    fix: issue with getUserIgnored params being swapped

--- modules/ept-messages/hooks/index.js
+++ modules/ept-messages/hooks/index.js
@@ -7,7 +7,7 @@ function checkUserIgnoredMessages(request) {
   if (request.auth.isAuthenticated) { authedUserId = request.auth.credentials.id; }
   else { return Boom.unauthorized('You must log in to perform this action'); }
   return Promise.each(receivers, function(receiverId) {
-    return request.db.messages.getUserIgnored(authedUserId, receiverId)
+    return request.db.messages.getUserIgnored(receiverId, authedUserId)
     .then(function(data) {
       if (data.ignored) { return Promise.reject(Boom.forbidden('One or more of the users you are trying to send a message to has choosen to ignore you.')); }
       else { return true; }
