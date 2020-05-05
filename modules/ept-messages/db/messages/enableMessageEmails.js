e87603100371d839d258e19b2160fa766917383f
Anthony Kinsey <anthonyjk@gmail.com>
2020-05-04 14:42:55 -1000

    fix: issue with message settings not updating

--- modules/ept-messages/db/messages/enableMessageEmails.js
+++ modules/ept-messages/db/messages/enableMessageEmails.js
@@ -5,9 +5,9 @@ var db = dbc.db;
 
 module.exports = function(userId, enabled) {
   userId = helper.deslugify(userId);
-  var q = `INSERT INTO users.preferences (user_id, email_mentions)
+  var q = `INSERT INTO users.preferences (user_id, email_messages)
            VALUES ($1, $2)
-           ON CONFLICT (user_id) DO UPDATE SET email_mentions = $2`;
+           ON CONFLICT (user_id) DO UPDATE SET email_messages = $2`;
   var params = [ userId, enabled ];
   return db.sqlQuery(q, params)
   .then(function() { return { enabled: enabled }; });
