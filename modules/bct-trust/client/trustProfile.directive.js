627d39cb00a1dfa3bc1fd78f649e39ce3dad01e2
Ryan Uesato <ruesato@gmail.com>
2020-05-08 14:30:13 -1000

    fix: Fixed mobile layout of User Profile view

--- modules/bct-trust/client/trustProfile.directive.js
+++ modules/bct-trust/client/trustProfile.directive.js
@@ -1,6 +1,6 @@
 var html = '<div><span>Trust:&nbsp;&nbsp;<strong><span class="trust-score" ng-class="vmTrust.getStyle(vmTrust.stats.score)"><span data-balloon="Trust Score">{{vmTrust.stats.score}}</span> : <span data-balloon="Negative Feedback" ng-class="{\'neg\' : vmTrust.stats.neg !== 0 }">-{{vmTrust.stats.neg}}</span> / <span data-balloon="Positive Feedback">+{{vmTrust.stats.pos}}</span></span></strong></span></div>';
 html += '<div ng-if="vmTrust.stats.score < 0"><span class="trust-score" ng-class="vmTrust.getStyle(vmTrust.stats.score)">Warning: Trade with extreme caution!</span></div>';
-html += '<div><span><a ui-sref="trust({ username: vmTrust.username })">View Trust Feedback</a></span></div>';
+html += '<div><span><a ui-sref="trust({ username: vmTrust.username })" class="trust-link">View Trust Feedback</a></span></div>';
 
 var directive = ['UserTrust', function(UserTrust) {
   return {
