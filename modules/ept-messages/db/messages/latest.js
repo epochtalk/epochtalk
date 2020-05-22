4ce5c5459f2496aa9b852f4a6bd02d3c754b8b26
Anthony Kinsey <anthonyjk@gmail.com>
2020-05-22 12:42:18 -1000

    fix: show newbie alert when page refreshes after nessage reply

--- modules/ept-messages/db/messages/latest.js
+++ modules/ept-messages/db/messages/latest.js
@@ -8,7 +8,7 @@ module.exports = function(userId, opts) {
   userId = helper.deslugify(userId);
   opts = opts || {};
 
-  var columns = 'mid.id, mid.conversation_id, mid.sender_id, mid.receiver_ids, mid.content, mid.created_at, mid.read_by_user_ids, s.username as sender_username, s.deleted as sender_deleted, s.avatar as sender_avatar';
+  var columns = 'mid.id, mid.conversation_id, mid.sender_id, mid.receiver_ids, mid.content, mid.created_at, mid.read_by_user_ids, s.username as sender_username, s.deleted as sender_deleted, s.avatar as sender_avatar, s.newbie_alert as sender_newbie_alert';
   var q = ` SELECT * FROM
     ( SELECT
       DISTINCT ON (conversation_id) conversation_id, id, sender_id,
@@ -19,7 +19,7 @@ module.exports = function(userId, opts) {
       AND (sender_id = $1 OR $1 = ANY(receiver_ids))
       ORDER BY conversation_id, created_at DESC )
       AS m ORDER BY m.created_at DESC LIMIT $2 OFFSET $3`;
-  var q2 = 'SELECT u.username, u.deleted, up.avatar FROM users u LEFT JOIN users.profiles up ON u.id = up.user_id WHERE u.id = mid.sender_id';
+  var q2 = 'SELECT CASE WHEN EXISTS (SELECT ru.user_id FROM roles_users ru WHERE ru.user_id = mid.sender_id AND ru.role_id = \'08dd21e5-9781-4c6a-8c6f-3c1574c59a85\') THEN \'TRUE\'::boolean ELSE \'FALSE\'::boolean END AS newbie_alert, u.username, u.deleted, up.avatar FROM users u LEFT JOIN users.profiles up ON u.id = up.user_id WHERE u.id = mid.sender_id';
   var query = 'SELECT ' + columns + ' FROM ( ' +
     q + ' ) mid LEFT JOIN LATERAL ( ' +
     q2 + ' ) s ON true ORDER BY mid.created_at DESC';
