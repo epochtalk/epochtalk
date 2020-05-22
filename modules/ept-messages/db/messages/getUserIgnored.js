ce1f6d5bfda0402d388e85130250e199a1dfe288
Anthony Kinsey <anthonyjk@gmail.com>
2020-05-21 14:05:53 -1000

    fix: fixes #831, issue with wrong receiver name in error when blocking messages from a users

--- modules/ept-messages/db/messages/getUserIgnored.js
+++ modules/ept-messages/db/messages/getUserIgnored.js
@@ -7,7 +7,7 @@ module.exports = function(authedUserId, receiverId) {
   authedUserId = helper.deslugify(authedUserId);
   receiverId = helper.deslugify(receiverId);
   // Check if sender is ignoring receiver
-  var q = 'SELECT ignored_user_id, u.username as receiver_username, (CASE WHEN (user_id = $1 AND ignored_user_id = $2) THEN \'TRUE\'::boolean WHEN (user_id = $2 AND ignored_user_id = $1) THEN \'FALSE\'::boolean END) as ignored_by_authed FROM messages.ignored LEFT JOIN users u ON u.id = user_id WHERE (user_id = $1 AND ignored_user_id = $2) OR (user_id = $2 AND ignored_user_id = $1)';
+  var q = 'SELECT ignored_user_id, (SELECT username FROM users WHERE id = $2) as receiver_username, (CASE WHEN (user_id = $1 AND ignored_user_id = $2) THEN \'TRUE\'::boolean WHEN (user_id = $2 AND ignored_user_id = $1) THEN \'FALSE\'::boolean END) as ignored_by_authed FROM messages.ignored LEFT JOIN users u ON u.id = user_id WHERE (user_id = $1 AND ignored_user_id = $2) OR (user_id = $2 AND ignored_user_id = $1)';
   return db.scalar(q, [authedUserId, receiverId])
   .then(function(data) {
     if (data) {
