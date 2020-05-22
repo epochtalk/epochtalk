5fbb2c1e1cbc84c774f08a47a5753b9bd9fac2a5
Anthony Kinsey <anthonyjk@gmail.com>
2020-05-21 15:51:03 -1000

    fix: issue with all convo members not being listed, fixes #832

--- modules/ept-messages/authorization/createMessage.js
+++ modules/ept-messages/authorization/createMessage.js
@@ -14,6 +14,35 @@ module.exports = function messagesCreate(server, auth, receiverIds, convoId) {
     permission: 'messages.create.allow'
   });
 
+  // check if user is ignoring newbies, if they are they cannot send messages to newbies
+  var canMessageNewbies = server.db.messages.getMessageSettings(userId)
+  .then(function(data) {
+    if (data) { return data.ignore_newbies; }
+    else { return Promise.reject(Boom.badRequest('There was an error starting your conversation')); }
+  })
+  .then(function(isIgnoring) {
+    if (isIgnoring) {
+      return Promise.all(Promise.map(receiverIds, function(receiverId) {
+        return server.db.users.find(receiverId)
+        .then(function(refUser) {
+          var refIsNewbie = false;
+          for(var i = 0; i < refUser.roles.length; i++) {
+            var roleLookup = refUser.roles[i].lookup;
+            if (roleLookup === 'newbie') {
+              refIsNewbie = true;
+              break;
+            }
+          }
+          if (refIsNewbie) {
+            return Promise.reject(Boom.forbidden(refUser.username + ' is a newbie, you must stop ignoring newbie messages in order to send this message'));
+          }
+          else { return Promise.resolve(true); }
+        });
+      }));
+    }
+    else { return Promise.resolve(true); }
+  });
+
   // is a member of the conversation
   var convoMember = server.db.conversations.isConversationMember(convoId, userId)
   .then(function(isMember) {
@@ -37,6 +66,6 @@ module.exports = function messagesCreate(server, auth, receiverIds, convoId) {
     }));
   }
 
-  return Promise.all([allowed, convoMember, priority]);
+  return Promise.all([allowed, convoMember, priority, canMessageNewbies]);
 };
 
