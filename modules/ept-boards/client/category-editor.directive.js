5cbace52370909e548b62841870d35ca973605ab
Anthony Kinsey <anthonyjk@gmail.com>
2020-06-25 12:35:13 -1000

    feat: update category editor directive to support new selfmod option

--- modules/ept-boards/client/category-editor.directive.js
+++ modules/ept-boards/client/category-editor.directive.js
@@ -38,6 +38,7 @@ var directive = ['$state', function($state) {
         $('#newBoardPostable').val('');
         $('#ltr').prop('checked', true);
         $('#enable-signature').prop('checked', true);
+        $('#enable-selfmod').prop('checked', true);
         $('#newBoardPostEdit').val('');
       };
 
@@ -116,6 +117,12 @@ var directive = ['$state', function($state) {
         else {
           $('#enable-signature-edit').prop('checked', true);
         }
+        if (editBoard.disable_selfmod) {
+          $('#disable-selfmod-edit').prop('checked', true);
+        }
+        else {
+          $('#enable-selfmod-edit').prop('checked', true);
+        }
         if (editBoard.disable_post_edit === "true") {
           //shim for old true values, post editing immediately disabled
           $('#editBoardPostEdit').val(0);
@@ -140,6 +147,7 @@ var directive = ['$state', function($state) {
               newBoard.disable_post_edit = $('#editBoardPostEdit"]').val();
               newBoard.right_to_left = $('input[name="editBoardRTL"]:checked').val() === "true";
               newBoard.disable_signature = $('input[name="editBoardSignatures"]:checked').val() === "true";
+              newBoard.disable_selfmod = $('input[name="editBoardSelfMod"]:checked').val() === "true";
             }
           });
         }
@@ -153,7 +161,8 @@ var directive = ['$state', function($state) {
             postable_by: $('#editBoardPostable').val() || null,
             disable_post_edit: $('#editBoardPostEdit').val(),
             right_to_left: $('input[name="editBoardRTL"]:checked').val() === "true",
-            disable_signature: $('input[name="editBoardSignatures"]:checked').val() === "true"
+            disable_signature: $('input[name="editBoardSignatures"]:checked').val() === "true",
+            disable_selfmod: $('input[name="editBoardSelfMod"]:checked').val() === "true"
           };
 
           // check if board already exists in editedBoards
@@ -168,6 +177,7 @@ var directive = ['$state', function($state) {
             foundBoard.right_to_left = editedBoard.right_to_left;
             foundBoard.disable_post_edit = editedBoard.disable_post_edit;
             foundBoard.disable_signature = editedBoard.disable_signature;
+            foundBoard.disable_selfmod = editedBoard.disable_selfmod;
           }
           else { $scope.editedBoards.push(editedBoard); }
         }
@@ -189,6 +199,7 @@ var directive = ['$state', function($state) {
         board.disable_post_edit = $('#editBoardPostEdit').val();
         board.right_to_left = $('input[name="editBoardRTL"]:checked').val() === "true";
         board.disable_signature = $('input[name="editBoardSignatures"]:checked').val() === "true";
+        board.disable_selfmod = $('input[name="editBoardSelfMod"]:checked').val() === "true";
 
         // Reset scope params for editing board
         editBoardDataId = '';
@@ -199,6 +210,7 @@ var directive = ['$state', function($state) {
         $('#editBoardPostable').val('');
         $('#ltr-edit').prop('checked', true);
         $('#enable-signature-edit').prop('checked', true);
+        $('#enable-selfmod-edit').prop('checked', true);
         $('#editBoardPostEdit').val('');
         $scope.showEditBoard = false;
       };
@@ -342,6 +354,7 @@ var directive = ['$state', function($state) {
               postable_by: $scope.nestableMap[catBoard.id].postable_by,
               right_to_left: $scope.nestableMap[catBoard.id].right_to_left,
               disable_signature: $scope.nestableMap[catBoard.id].disable_signature,
+              disable_selfmod: $scope.nestableMap[catBoard.id].disable_selfmod,
               disable_post_edit: $scope.nestableMap[catBoard.id].disable_post_edit,
               view_order: index
             };
@@ -368,6 +381,7 @@ var directive = ['$state', function($state) {
             postable_by: $scope.nestableMap[board.id].postable_by,
             right_to_left: $scope.nestableMap[board.id].right_to_left,
             disable_signature: $scope.nestableMap[board.id].disable_signature,
+            disable_selfmod: $scope.nestableMap[board.id].disable_selfmod,
             disable_post_edit: $scope.nestableMap[board.id].disable_post_edit,
             view_order: index
           };
