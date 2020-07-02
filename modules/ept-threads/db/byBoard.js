1a888bb48ddba7f1b87c180a5c07109db5a372a9
Anthony Kinsey <anthonyjk@gmail.com>
2020-07-02 13:38:33 -1000

    fix: fix issue with sort order being wrong when changing pages

--- modules/ept-threads/db/byBoard.js
+++ modules/ept-threads/db/byBoard.js
@@ -15,6 +15,7 @@ module.exports = function(boardId, userId, opts) {
   opts.offset = (opts.page * opts.limit) - opts.limit;
   opts.sortField = opts.sortField || 'updated_at';
   opts.reversed = opts.desc ? 'DESC' : '';
+  opts.sortOrder = opts.reversed;
   opts.columns = 'tlist.id, t.locked, t.sticky, t.moderated, t.poll, t.created_at, t.updated_at, tlist.views AS view_count, t.post_count, p.title, p.user_id, p.username, p.user_deleted, t.time AS last_viewed, tv.id AS post_id, tv.position AS post_position, pl.last_post_id, pl.position AS last_post_position, pl.created_at AS last_post_created_at, pl.deleted AS last_post_deleted, pl.id AS last_post_user_id, pl.username AS last_post_username, pl.user_deleted AS last_post_user_deleted, pl.avatar AS last_post_avatar';
   opts.q2 = 'SELECT t1.locked, t1.sticky, t1.moderated, t1.post_count, t1.created_at, t1.updated_at, mt.views, ' +
     '(SELECT EXISTS ( SELECT 1 FROM polls WHERE thread_id = tlist.id )) as poll, ' +
@@ -73,7 +74,7 @@ var getNormalThreads = function(boardId, userId, opts) {
     'LEFT JOIN LATERAL ( ' + opts.q3 + ' ) p ON true ' +
     'LEFT JOIN LATERAL ( ' + opts.q4 + ' ) tv ON true ' +
     'LEFT JOIN LATERAL ( ' + opts.q5 + ' ) pl ON true ' +
-    'ORDER BY tlist.' + opts.sortField + ' DESC';
+    'ORDER BY tlist.' + opts.sortField + ' ' + opts.sortOrder;
     console.log(query);
     var params = [boardId, userId, opts.limit, opts.offset];
     console.log(params);
