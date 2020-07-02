4cad1694d072fa3c601b09ca8ebd7b8537415b11
Anthony Kinsey <anthonyjk@gmail.com>
2020-07-02 11:48:16 -1000

    feat: wip fix sorting by views

--- modules/ept-threads/db/byBoard.js
+++ modules/ept-threads/db/byBoard.js
@@ -14,8 +14,8 @@ module.exports = function(boardId, userId, opts) {
   opts.page = opts.page || 1;
   opts.offset = (opts.page * opts.limit) - opts.limit;
   opts.sortField = opts.sortField || 'updated_at';
-  opts.reversed = opts.desc ? 'DESC' : 'ASC';
-  opts.columns = 'tlist.id, t.locked, t.sticky, t.moderated, t.poll, t.created_at, t.updated_at, t.views as view_count, t.post_count, p.title, p.user_id, p.username, p.user_deleted, t.time AS last_viewed, tv.id AS post_id, tv.position AS post_position, pl.last_post_id, pl.position AS last_post_position, pl.created_at AS last_post_created_at, pl.deleted AS last_post_deleted, pl.id AS last_post_user_id, pl.username AS last_post_username, pl.user_deleted AS last_post_user_deleted, pl.avatar AS last_post_avatar';
+  opts.reversed = opts.desc ? 'DESC' : '';
+  opts.columns = 'tlist.id, t.locked, t.sticky, t.moderated, t.poll, t.created_at, t.updated_at, tlist.views AS view_count, t.post_count, p.title, p.user_id, p.username, p.user_deleted, t.time AS last_viewed, tv.id AS post_id, tv.position AS post_position, pl.last_post_id, pl.position AS last_post_position, pl.created_at AS last_post_created_at, pl.deleted AS last_post_deleted, pl.id AS last_post_user_id, pl.username AS last_post_username, pl.user_deleted AS last_post_user_deleted, pl.avatar AS last_post_avatar';
   opts.q2 = 'SELECT t1.locked, t1.sticky, t1.moderated, t1.post_count, t1.created_at, t1.updated_at, mt.views, ' +
     '(SELECT EXISTS ( SELECT 1 FROM polls WHERE thread_id = tlist.id )) as poll, ' +
     '(SELECT time FROM users.thread_views WHERE thread_id = tlist.id AND user_id = $2) ' +
@@ -53,7 +53,7 @@ var getNormalThreads = function(boardId, userId, opts) {
         threadCount = threadCount - stickyThreadCountResult.count;
       }
       if (opts.offset > Math.floor(threadCount / 2)) {
-        opts.reversed = '';
+        opts.reversed = opts.reversed === '' ? 'DESC' : '';
         opts.limit = threadCount <= opts.offset + opts.limit ? threadCount - opts.offset : opts.limit;
         opts.offset = threadCount <= opts.offset + opts.limit ? 0 : threadCount - opts.offset - opts.limit;
       }
@@ -74,7 +74,9 @@ var getNormalThreads = function(boardId, userId, opts) {
     'LEFT JOIN LATERAL ( ' + opts.q4 + ' ) tv ON true ' +
     'LEFT JOIN LATERAL ( ' + opts.q5 + ' ) pl ON true ' +
     'ORDER BY tlist.' + opts.sortField + ' DESC';
+    console.log(query);
     var params = [boardId, userId, opts.limit, opts.offset];
+    console.log(params);
     return db.sqlQuery(query, params);
   })
   .then(function(threads) {
@@ -87,6 +89,7 @@ var getNormalThreads = function(boardId, userId, opts) {
 
 var getStickyThreads = function(boardId, userId, opts) {
   if (opts.page !== 1) { return []; }
+  opts.columns = opts.columns.replace('tlist.views', 't.views');
   var query = 'SELECT ' + opts.columns + ' FROM ( ' +
     'SELECT id, created_at, updated_at ' +
     'FROM threads ' +
