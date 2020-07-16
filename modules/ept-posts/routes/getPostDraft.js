de9a2986cddc895018e725dae76d2e7dcf81ceb2
Anthony Kinsey <anthonyjk@gmail.com>
2020-07-16 13:43:41 -1000

    refactor: wip create route to query user's latest post draft

---
+++ modules/ept-posts/routes/getPostDraft.js
@@ -0,0 +1,48 @@
+var Joi = require('@hapi/joi');
+var path = require('path');
+
+/**
+  * @apiVersion 0.4.0
+  * @apiGroup Posts
+  * @api {GET} /posts/drafts Find
+  * @apiName GetPostDraft
+  * @apiDescription Used to find a user's post draft.
+  *
+  * @apiUse PostObjectSuccess
+  * @apiSuccess {string} user_id The id of the user
+  * @apiSuccess {string} draft The user's latest draft
+  * @apiSuccess {timestamp} updated_at The updated at timestamp of the draft
+  *
+  * @apiError (Error 500) InternalServerError There was an issue finding the post
+  */
+module.exports = {
+  method: 'GET',
+  path: '/api/posts/draft',
+  options: {
+    app: { hook: 'posts.getPostDraft' },
+    auth: { strategy: 'jwt' },
+    pre: [
+      { method: (request) => request.server.methods.hooks.preProcessing(request) },
+      [
+        { method: (request) => request.server.methods.hooks.parallelProcessing(request), assign: 'parallelProcessed' },
+        { method: processing, assign: 'processed' },
+      ],
+      { method: (request) => request.server.methods.hooks.merge(request) },
+      { method: (request) => request.server.methods.hooks.postProcessing(request) }
+    ],
+    handler: function(request) {
+      return request.pre.processed;
+    }
+  }
+};
+
+function processing(request) {
+  // retrieve post draft
+  var userId = '';
+  var authenticated = request.auth.isAuthenticated;
+  if (authenticated) { userId = request.auth.credentials.id; }
+  var promise = request.db.posts.getPostDraft(userId)
+  .error(request.errorMap.toHttpError);
+
+  return promise;
+}
