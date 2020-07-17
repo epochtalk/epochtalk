bbf1e08b943a67456ba87671011c6756e4ade394
Anthony Kinsey <anthonyjk@gmail.com>
2020-07-17 11:24:26 -1000

    refactor: simplify db code for updatePostDraft

--- modules/ept-posts/db/updatePostDraft.js
+++ modules/ept-posts/db/updatePostDraft.js
@@ -9,8 +9,5 @@ module.exports = function(userId, draft) {
   userId = helper.deslugify(userId);
   var q = 'INSERT INTO posts.user_drafts (user_id, draft, updated_at) VALUES ($1, $2, now()) ON CONFLICT (user_id) DO UPDATE SET draft = $2, updated_at = now() RETURNING *';
   return db.scalar(q, [userId, draft])
-  .then(function(data) {
-    return data;
-  })
   .then(helper.slugify);
 };
