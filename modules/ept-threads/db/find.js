14ed105e2ecd55d01af40ad627bad56bcca29cf5
Anthony Kinsey <anthonyjk@gmail.com>
2020-08-07 12:44:51 -1000

    refactor: modify db methods to accept slug or id

--- modules/ept-threads/db/find.js
+++ modules/ept-threads/db/find.js
@@ -6,14 +6,21 @@ var helper = dbc.helper;
 var errors = dbc.errors;
 var NotFoundError = errors.NotFoundError;
 
-module.exports = function(id) {
-  id = helper.deslugify(id);
+module.exports = function(id, slug) {
   var columns = 't.id, t.board_id, t.locked, t.sticky, t.moderated, t.created_at, t.updated_at, t.post_count, p.user_id, p.title, p.username, p.user_deleted';
-  var q1 = 'SELECT id, board_id, locked, sticky, moderated, post_count, created_at, updated_at FROM threads WHERE id = $1';
+  var q1, params;
+  if (id) {
+    id = helper.deslugify(id);
+    q1 = 'SELECT id, board_id, locked, sticky, moderated, post_count, created_at, updated_at FROM threads WHERE id = $1';
+    params = [id];
+  }
+  else {
+    q1 = 'SELECT id, board_id, locked, sticky, moderated, post_count, created_at, updated_at FROM threads WHERE slug = $1';
+    params = [slug];
+  }
   var q2 = 'SELECT p1.user_id, p1.content ->> \'title\' as title, u.username, u.deleted as user_deleted FROM posts p1 LEFT JOIN users u on p1.user_id = u.id WHERE p1.thread_id = t.id ORDER BY p1.created_at limit 1';
   var query = 'SELECT ' + columns + ' FROM ( ' + q1 + ') t LEFT JOIN LATERAL ( ' + q2 + ' ) p ON true';
 
-  var params = [id];
   return db.sqlQuery(query, params)
   .then(function(rows) {
     if (rows.length > 0) { return rows[0]; }
--- modules/ept-threads/db/getThreadFirstPost.js
+++ modules/ept-threads/db/getThreadFirstPost.js
@@ -5,10 +5,18 @@ var helper = dbc.helper;
 var errors = dbc.errors;
 var NotFoundError = errors.NotFoundError;
 
-module.exports = function(threadId) {
-  threadId = helper.deslugify(threadId);
-  var q = 'SELECT id, content ->> \'title\' as title, content ->> \'body\' as body, thread_id FROM posts WHERE thread_id = $1 ORDER BY created_at LIMIT 1';
-  return db.sqlQuery(q, [threadId])
+module.exports = function(threadId, slug) {
+  var q, params;
+  if (threadId) {
+    threadId = helper.deslugify(threadId);
+    var q = 'SELECT p.id, p.content ->> \'title\' as title, p.content ->> \'body\' as body, p.thread_id, u.username FROM posts p LEFT JOIN users u ON u.id = p.user_id WHERE p.thread_id = $1 ORDER BY p.created_at LIMIT 1';
+    params = [threadId];
+  }
+  else {
+    var q = 'SELECT p.id, p.content ->> \'title\' as title, p.content ->> \'body\' as body, p.thread_id, u.username FROM posts p LEFT JOIN users u ON u.id = p.user_id WHERE p.slug = $1 ORDER BY p.created_at LIMIT 1';
+    params = [slug];
+  }
+  return db.sqlQuery(q, params)
   .then(function(rows) {
     if (rows.length > 0) { return rows[0]; }
     else { return new NotFoundError('Thread Not Found'); }
