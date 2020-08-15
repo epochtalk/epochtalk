b3713ca3fe9b0a7d1e918ee1d58c119615edda51
Anthony Kinsey <anthonyjk@gmail.com>
2020-08-14 14:05:47 -1000

    refactor: modify moderation logs to work with thread slugs

--- modules/ept-threads/db/find.js
+++ modules/ept-threads/db/find.js
@@ -7,15 +7,15 @@ var errors = dbc.errors;
 var NotFoundError = errors.NotFoundError;
 
 module.exports = function(id, slug) {
-  var columns = 't.id, t.board_id, t.locked, t.sticky, t.moderated, t.created_at, t.updated_at, t.post_count, p.user_id, p.title, p.username, p.user_deleted';
+  var columns = 't.id, t.board_id, t.slug, t.locked, t.sticky, t.moderated, t.created_at, t.updated_at, t.post_count, p.user_id, p.title, p.username, p.user_deleted';
   var q1, params;
   if (id) {
     id = helper.deslugify(id);
-    q1 = 'SELECT id, board_id, locked, sticky, moderated, post_count, created_at, updated_at FROM threads WHERE id = $1';
+    q1 = 'SELECT id, board_id, slug, locked, sticky, moderated, post_count, created_at, updated_at FROM threads WHERE id = $1';
     params = [id];
   }
   else {
-    q1 = 'SELECT id, board_id, locked, sticky, moderated, post_count, created_at, updated_at FROM threads WHERE slug = $1';
+    q1 = 'SELECT id, board_id, slug, locked, sticky, moderated, post_count, created_at, updated_at FROM threads WHERE slug = $1';
     params = [slug];
   }
   var q2 = 'SELECT p1.user_id, p1.content ->> \'title\' as title, u.username, u.deleted as user_deleted FROM posts p1 LEFT JOIN users u on p1.user_id = u.id WHERE p1.thread_id = t.id ORDER BY p1.created_at limit 1';
