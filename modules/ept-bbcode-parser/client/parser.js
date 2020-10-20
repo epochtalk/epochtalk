65140da9e02c21f1dac766fd01efae741598795e
Anthony Kinsey <anthonyjk@gmail.com>
2020-10-19 15:48:58 -1000

    fix: issue with bbcode rendering inside markdown code tag

--- modules/ept-bbcode-parser/client/parser.js
+++ modules/ept-bbcode-parser/client/parser.js
@@ -6,20 +6,22 @@ module.exports = {
   parse: function (input, sanitize) {
     if (!input) { input = ''; }
 
+    var regex = /(\[code\]([\s\S]*?)\[\/code\])|(```([\s\S]*?)```)/gi;
+    var codeTags = input.match(regex);
+    var replacedTagsInput = input;
+
+    var codeMatches = input.match(regex);
+    if (codeMatches && codeMatches.length) {
+      codeMatches.forEach(function(val) {
+        replacedTagsInput = replacedTagsInput.replace(val, '[code][/code]');
+      });
+    }
+
+    var cleanedText = replacedTagsInput;
     if (sanitize) {
       // used for posts and user signatures
       // display tags plus font, font face, font size
-      var regex = /\[code\]([\s\S]*?)\[\/code\]/gi;
-      var codeTags = input.match(regex);
-      var replacedTagsInput = input;
-
-      var codeMatches = input.match(regex);
-      if (codeMatches && codeMatches.length) {
-        codeMatches.forEach(function(val) {
-          replacedTagsInput = replacedTagsInput.replace(val, '[code][/code]');
-        });
-      }
-      var text = sanitizeHtml(replacedTagsInput, {
+      cleanedText = sanitizeHtml(replacedTagsInput, {
         allowedTags: [ 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol', 'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div', 'table', 'thead', 'tfoot', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img', 'sub', 'sup', 'tt', 'del' ],
         allowedAttributes: {
           a: [ 'href', 'name', 'target' ],
@@ -30,18 +32,24 @@ module.exports = {
         // URL schemes we permit
         allowedSchemes: [ 'http', 'https', 'ftp', 'mailto' ]
       });
-
-      var cleanedText = text;
-      var i = 0;
-      var matches = text.match(regex);
-      if (matches && matches.length) {
-        matches.forEach(function(val) {
-          cleanedText = cleanedText.replace(val, codeTags[i++]);
-        });
-      }
-
-      input = cleanedText;
     }
+
+    var i = 0;
+    var matches = cleanedText.match(regex);
+    if (matches && matches.length) {
+      matches.forEach(function(val) {
+        var replacee = codeTags[i++];
+        // Stop bbcode inside of markdown code tag from being proccessed
+        if (replacee.indexOf('```') === 0 && replacee.lastIndexOf('```') === (replacee.length - 3)) {
+          replacee = replacee.replace(/```/, '[code]');
+          replacee = replacee.slice(0, -3) + '[/code]';
+        }
+        console.log(replacee);
+        cleanedText = cleanedText.replace(val, replacee);
+      });
+    }
+
+    input = cleanedText;
 
     // Stop html entities from being encoded by replacing & with entity
     input = input.replace(/(?:&)/g, '&#38;');
