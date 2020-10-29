ab3e41871354029290665bbe53be8513f80ef324
Bronson Oka <boka@slickage.com>
2020-10-29 11:28:25 -1000

    style: indent promise in saveImage

--- modules/ept-images/plugins/s3.js
+++ modules/ept-images/plugins/s3.js
@@ -258,25 +258,25 @@ s3.uploadPolicy = function(filename) {
 
 s3.saveImage = function(imgSrc) {
   return new Promise(function(resolve, reject) {
-  // image uploaded by client
-  if (imgSrc.indexOf(config.images.s3.root) === 0) {
-    // clear any expirations
-    images.clearExpiration(imgSrc);
-    // return original image url
-    return imgSrc;
-  }
-  // hotlink image
-  else {
-    var filename = images.generateHotlinkFilename(imgSrc);
-    return uploadImage(imgSrc, filename)
-    .then(function() {
-      // return generated image url
-      return generateImageUrl(filename);
-    })
-    .catch(function(error) {
-      console.log('ept-images/plugins/s3', error);
-    });
-  }
+    // image uploaded by client
+    if (imgSrc.indexOf(config.images.s3.root) === 0) {
+      // clear any expirations
+      images.clearExpiration(imgSrc);
+      // return original image url
+      return imgSrc;
+    }
+    // hotlink image
+    else {
+      var filename = images.generateHotlinkFilename(imgSrc);
+      return uploadImage(imgSrc, filename)
+      .then(function() {
+        // return generated image url
+        return generateImageUrl(filename);
+      })
+      .catch(function(error) {
+        console.log('ept-images/plugins/s3', error);
+      });
+    }
   });
 };
 
