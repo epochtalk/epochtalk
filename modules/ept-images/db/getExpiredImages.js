9cf91045fd22f66446b9b679ed2f996904f65aa1
Bronson Oka <boka@slickage.com>
2021-11-16 10:54:44 -1000

    fix(images-expiry): query for expired images with UTC

--- modules/ept-images/db/getExpiredImages.js
+++ modules/ept-images/db/getExpiredImages.js
@@ -3,6 +3,6 @@ var dbc = require(path.normalize(__dirname + '/db'));
 var db = dbc.db;
 
 module.exports = function() {
-  var q = 'SELECT image_url FROM image_expirations WHERE expiration < now()';
+  var q = 'SELECT image_url FROM image_expirations WHERE expiration < timezone(\'utc\', now())';
   return db.sqlQuery(q);
 };
